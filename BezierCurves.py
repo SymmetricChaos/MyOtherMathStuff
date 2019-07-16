70edbdd909ae6a2665591dc79c9b5b76b5c3615f
SymmetricChaos <ajfraebel@gmail.com>
2019-07-15 22:23:22 -0400

    Update BezierCurves.py
    More natural input method

--- BezierCurves.py
+++ BezierCurves.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from Utils import make_canvas
 
 def interpolation(A,B,p):
     """The point that is some exactly p percent of the way between A and B"""
@@ -18,34 +19,38 @@ def bezier_quadratic(begin=[0,0],end=[0,0],control=[0,0],N=50):
     t = np.linspace(0,1,N)
     P0 = interpolation(begin,control,t)
     P1 = interpolation(control,end,t)
-#    plt.plot(P0[0],P0[1])
-##    plt.plot(P1[0],P1[1])
     X,Y = interpolation(P0,P1,t)
+#    plt.plot(P0[0],P0[1])
+#    plt.plot(P1[0],P1[1])
+#    plt.plot(X,Y)
     return X,Y
     
 
-def bezier(L,N=50):
+def bezier(begin,end,control,N=50):
     """
     Bezier curves of any complexity are possible
     They interpolate between several lines or, equivalently, between several Bezier curves
     Points are provided in sequence
     """
-    Qold = L.copy()
+    L = [begin] + control + [end]
     while True:
         P = []
-        if len(Qold) == 1:
-            return Qold[0]
-        for i in range(len(Qold)-1):
+        if len(L) == 1:
+            return L[0]
+        for i in range(len(L)-1):
             t = np.linspace(0,1,N)
-            P.append(interpolation(Qold[i],Qold[i+1],t))
+            P.append(interpolation(L[i],L[i+1],t))
 
-        Qold = P.copy()
+        L = P.copy()
         
 
-pts = [[-2,0],[0,1.5],[.5,-1],[2,0]]
-XY = bezier(pts)
-plt.plot(XY[0],XY[1])
-plt.scatter(pts[0][0],pts[0][1])
-plt.scatter(pts[1][0],pts[1][1])
-plt.scatter(pts[2][0],pts[2][1])
-plt.scatter(pts[3][0],pts[3][1])
+fig,ax = make_canvas([-2.5,2.5],[-2.5,2.5])
+
+pts = [[0,1.5],[.5,-1]]
+
+X,Y = bezier([-2,0],[2,0],pts)
+
+plt.plot(X,Y)
+
+plt.scatter(pts[0][0],pts[0][1],color='k')
+plt.scatter(pts[1][0],pts[1][1],color='k')
