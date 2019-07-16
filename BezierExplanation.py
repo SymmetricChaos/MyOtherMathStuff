9189a3bf56c44173217769b0f8256b5cf7a789b7
SymmetricChaos <ajfraebel@gmail.com>
2019-07-16 07:18:51 -0400

    Explanation Created

--- BezierExplanation.py
+++ BezierExplanation.py
@@ -1,18 +1,23 @@
 import matplotlib.pyplot as plt
-from Utils import make_canvas
+from Utils import make_canvas, tuples_to_lists
 from BezierCurves import bezier
 
 fig,ax = make_canvas([-2.5,2.5],[-2.5,2.5])
 
 begin = [-2,0]
 end = [2,0]
-control = [[.5,-1],[0,1.5]]
+control = [[-1,1],[0,1],[1,1]]
 
 X,Y = bezier(begin,end,control)
 
 plt.plot(X,Y,zorder=0)
 
+
 plt.scatter(begin[0],begin[1],color='r')
 plt.scatter(end[0],end[1],color='r')
 for pt in control:
     plt.scatter(pt[0],pt[1],color='k')
+    
+
+a = tuples_to_lists([begin]+control+[end]+[begin])
+plt.plot(a[0],a[1],zorder=-1,color='gray')
--- Utils.py
+++ Utils.py
@@ -6,3 +6,19 @@ def make_canvas(x=[-3,3],y=[-3,3],size=[7,7]):
     ax = plt.axes(xlim=x, ylim=y)
     ax.axis('off')
     return fig, ax
+
+def lists_to_tuples(*args):
+    """Convert lists to a list of tuples"""
+    L = []
+    for i in zip(*args):
+        L.append(i)
+    return L
+
+
+def tuples_to_lists(L):
+    """Convert a list of tuples to lists"""
+    out = []
+    W = len(L[0])
+    for w in range(W):
+        out.append( [i[w] for i in L] )
+    return out
