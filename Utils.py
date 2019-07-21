93b8816a78b5b3800553f3a698065866f6f17818
SymmetricChaos <ajfraebel@gmail.com>
2019-07-21 09:37:57 -0400

    Update Utils.py
    Easier to use when making a square

--- Utils.py
+++ Utils.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 
-def make_canvas(x=[-3,3],y=[-3,3],size=[7,7]):
+def make_canvas(x,y=None,size=[7,7]):
+    if not y:
+        y = x
     fig = plt.figure()
     fig.set_size_inches(size[0], size[1])
     ax = plt.axes(xlim=x, ylim=y)
