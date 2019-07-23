51e0fcb02534b4d6d41225ac749beba1a05d3ce3
SymmetricChaos <ajfraebel@gmail.com>
2019-07-23 00:15:40 -0400

    Better default behavior

--- Utils/Drawing.py
+++ Utils/Drawing.py
@@ -1,8 +1,14 @@
 import matplotlib.pyplot as plt
 
-def make_canvas(x,y=None,size=[7,7]):
+def make_canvas(x,y=None,size=None):
     if not y:
         y = x
+    if not size:
+        xlim = abs(x[0]-x[1])
+        ylim = abs(y[0]-y[1])
+        xco = min(xlim,ylim)/ylim
+        yco = min(xlim,ylim)/xlim
+        size = [xco*7,yco*7]
     fig = plt.figure()
     fig.set_size_inches(size[0], size[1])
     ax = plt.axes(xlim=x, ylim=y)
