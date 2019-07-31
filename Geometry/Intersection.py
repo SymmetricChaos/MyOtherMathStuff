2d20d2b860e303788697a24fb4c5436f1c1d38c7
SymmetricChaos <ajfraebel@gmail.com>
2019-07-31 14:06:11 -0400

    Circle intersection

---
+++ Geometry/Intersection.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+fig = plt.figure(figsize=(8,8))
+plt.axis([-2,2,-2,2])
+
+def mxbline(mb=[0,0],x=[-2,2]):
+    y = [mb[0]*x[0]+mb[1],mb[0]*x[1]+mb[1]]
+    plt.plot(x,y)
+    
+    
+def intersect(m1,b1,m2,b2):
+    x = (b2-b1)/(m1-m2)
+    y = x*m1+b1
+    return x,y
+
+mb1 = [2,.5]
+mb2 = [-3,1]
+mxbline(mb1)
+mxbline(mb2)
+
+a = intersect(mb1[0],mb1[1],mb2[0],mb2[1])
+plt.plot(a[0],a[1],'ko')
+print(a)
--- Geometry/Shapes.py
+++ Geometry/Shapes.py
@@ -1,12 +1,24 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class Circle:
-    def __init__(self,r):
+    def __init__(self,r,pos=[0,0]):
         self.r = r
         self.cur = 1/r
         self.area = r*r*np.pi
+        self.pos = pos
     
-    def draw(self,pos=[0,0],color="black"):
+    def draw(self,color="black"):
         ax = plt.gca()
-        circ = plt.Circle(pos,radius = self.r, fc = "#00000000", ec = color)
+        circ = plt.Circle(self.pos,radius = self.r, fc = "#00000000", ec = color)
         ax.add_patch(circ)
+        
+#class Ellipse:
+#    def __init__(self,a,b,pos=[0,0]):
+#        self.a = a
+#        self.b = b
+#        self.area = np.pi*a*b
+#        self.ecc = 
+#        self.foci
+#        
+        
