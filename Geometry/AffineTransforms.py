08d5ca5995d44a984ff51ffbbac4354c5fbe1e45
SymmetricChaos <ajfraebel@gmail.com>
2019-08-04 09:01:39 -0400

    More transforms

--- Geometry/AffineTransforms.py
+++ Geometry/AffineTransforms.py
@@ -1,9 +1,22 @@
 import numpy as np
 
-def rot_mat(th):
-    return np.array([[np.cos(th),np.sin(th)],[-np.sin(th),np.cos(th)]])
-
-
 def rotate(P,th):
     assert type(P) == np.ndarray
-    return np.matmul(P,rot_mat(th))
+    M = np.array([[np.cos(th),np.sin(th)],[-np.sin(th),np.cos(th)]])
+    return np.matmul(P,M)
+
+def reflect_y(P):
+    assert type(P) == np.ndarray
+    M = np.array([[-1,0],[0,1]])
+    return np.matmul(P,M)
+
+def reflect_x(P):
+    assert type(P) == np.ndarray
+    M = np.array([[0,-1],[1,0]])
+    return np.matmul(P,M)
+
+
+def shear(P,x=0,y=0):
+    assert type(P) == np.ndarray
+    M = np.array([[1,y],[x,1]])
+    return np.matmul(P,M)
--- Geometry/ShapesExamples.py
+++ Geometry/ShapesExamples.py
@@ -1,7 +1,7 @@
 from Shapes import Circle, Ellipse
 from Utils.Drawing import make_canvas, plot_points, scatter_points
 import matplotlib.pyplot as plt
-from AffineTransforms import rotate
+from AffineTransforms import rotate, reflect_y
 import numpy as np
 
 
@@ -11,15 +11,17 @@ plot_points(c.points())
 plt.scatter(c.pos[0],c.pos[1])
 
 e = Ellipse(2.5,2)
-make_canvas([-3,3],size=4)
-plot_points(e.points())
-scatter_points(e.foci(),color='orange')
-
-
 E = e.points()
 E += e.foci()
 E = np.asarray(E)
-P = rotate(E,1)
+
+make_canvas([-3,3],size=4)
+plot_points(E[:-2])
+scatter_points(E[100:101],color='orange')
+scatter_points(E[101:102],color='green')
+
+P = reflect_y(rotate(E,1))
 make_canvas([-3,3],size=4)
 plot_points(P[:-2])
-scatter_points(P[-2:],color='orange')
+scatter_points(P[100:101],color='orange')
+scatter_points(P[101:102],color='green')
