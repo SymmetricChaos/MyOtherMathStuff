315229da5b1ef11976ec3893c70c530d48d28976
SymmetricChaos <ajfraebel@gmail.com>
2019-08-10 10:40:24 -0400

    modified bit strings

--- Information/BitstringType.py
+++ Information/BitstringType.py
@@ -2,18 +2,21 @@ from random import uniform, choices
 
 class Bitstring:
     
-    def __init__(self,bits=[]):
-        assert type(bits) == list
+    def __init__(self,bits=""):
+        assert type(bits) == str
         for i in bits:
-            if i not in [0,1]:
+            if i not in "01":
                 raise Exception(f"{i} {type(i)} is not a valid bit")
-        self.bits = bits
+        self.bits = [int(i) for i in bits]
 
     def __str__(self):
         out = ""
         for i in self.bits:
             out += str(i)
         return out
+    
+    def __int__(self):
+        return int(str(self), 2)
     
     def __len__(self):
         return len(self.bits)
@@ -34,7 +37,11 @@ class Bitstring:
                 self.bits[i] = (self.bits[i]+1)%2
                 
 def random_bitstring(n):
-    B = choices([0,1],k=n)
-    return Bitstring(B)
+    B = choices(["0","1"],k=n)
+    return Bitstring("".join(B))
 
-#def text_to_bitstyring(s):
+def text_to_bitstring(s):
+    out = ""
+    for let in s:
+        out += bin(ord("A"))[2:]
+    return Bitstring(out)
--- Information/RepetitionCode.py
+++ Information/RepetitionCode.py
@@ -1,22 +1,22 @@
-from BitstringType import Bitstring, random_bitstring
+from BitstringType import Bitstring, text_to_bitstring
 
-def repetition_encode(bits):
-    assert type(bits) == Bitstring
-    out = Bitstring()
-    for b in bits.bits:
-        out.bits += [b]*3
-    return out
+def repetition_encode(B):
+    assert type(B) == Bitstring
+    out = ""
+    for b in B.bits:
+        out += str(b)*3
+    return Bitstring(out)
 
 def repetition_decode(B):
     assert type(B) == Bitstring
     out = [B.bits[i * 3:(i + 1) * 3] for i in range((len(B.bits) + 2) // 3 )]
-    out = [0 if sum(i) < 2 else 1 for i in out]
-    return Bitstring(out)
+    out = ["0" if sum(i) < 2 else "1" for i in out]
+    return Bitstring("".join(out))
 
-b = random_bitstring(10)
-print(b)
+b = text_to_bitstring("The")
 ecc = repetition_encode(b)
 ecc.corrupt(.1)
 dec = repetition_decode(ecc)
+print(b)
 print(dec)
 print(b==dec)
