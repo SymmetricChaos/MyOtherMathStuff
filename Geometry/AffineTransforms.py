376879eca5c8f69ecbe58e6a1740d0c86912d3b8
SymmetricChaos <ajfraebel@gmail.com>
2019-08-13 17:43:47 -0400

    Update AffineTransforms.py

--- Geometry/AffineTransforms.py
+++ Geometry/AffineTransforms.py
@@ -16,7 +16,23 @@ def reflect_x(P):
     return np.matmul(P,M)
 
 
-def shear(P,x=0,y=0):
+def shear(P,x=1,y=1):
     assert type(P) == np.ndarray
     M = np.array([[1,y],[x,1]])
     return np.matmul(P,M)
+
+def scale(P,s=1):
+    assert type(P) == np.ndarray
+    M = np.array([[1,s],[s,1]])
+    return np.matmul(P,M)
+
+def reflect_line(P,m=1):
+    assert type(P) == np.ndarray
+    M = np.array([[1-m*m,2*m],[2*m,m*m-1]])
+    M /= 1+m*m
+    return np.matmul(P,M)
+
+def shift_xy(P,x=0,y=0):
+    assert type(P) == np.ndarray
+    M = np.array([x,y])
+    return P+M
