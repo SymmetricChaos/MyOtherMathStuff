18dd8ba565e4b80bc0ce09755435fd3fbc38735c
SymmetricChaos <ajfraebel@gmail.com>
2019-08-26 13:01:55 -0400

    SuperEllipse

--- Geometry/ShapesExamples.py
+++ Geometry/ShapesExamples.py
@@ -1,4 +1,5 @@
 from Shapes import Circle, Ellipse
+from SuperEllipse import SuperEllipse
 from Utils.Drawing import make_canvas, plot_points, scatter_points
 from AffineTransforms import rotate, reflect_y
 import numpy as np
@@ -22,3 +23,8 @@ plot_points(P[:-2])
 scatter_points(P[100:101],color='orange')
 scatter_points(P[101:102],color='green')
 
+A = SuperEllipse(a=2,b=2)
+for i in [.4,.8,1.2,1.6,2,2.4]:
+    make_canvas([-3,3],size=4)
+    A.n = i
+    A.draw()
---
+++ Geometry/SuperEllipse.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class SuperEllipse:
+    def __init__(self,a=1,b=1,n=.5,pos=[0,0]):
+        if b > a:
+            a,b = b,a
+        self.a = a
+        self.b = b
+        self.n = n
+        self.pos = pos
+        
+    def draw(self,p=100):
+        th = np.linspace(0,np.pi,p)
+        print()
+        x = self.a*np.cos(th)**(2/self.n)
+        y = self.b*np.sin(th)**(2/self.n)
+        
+        X = np.append(x,[-x,-x])
+        Y = np.append(y,[y,-y])
+
+        plt.plot(X[1:],Y[1:],color='black')
