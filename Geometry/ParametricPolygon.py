0f20d4c39ce140f917d072bc410ad9f130af93e4
SymmetricChaos <ajfraebel@gmail.com>
2019-08-28 20:12:48 -0400

    Minor
    Better example and better defaults

--- Geometry/ParametricPolygon.py
+++ Geometry/ParametricPolygon.py
@@ -1,11 +1,14 @@
 import numpy as np
 
-def parametric_polygon(n):
-    th = np.linspace(0,1,100)
+def parametric_polygon(n,r=1,pts=None):
+    if pts:
+        th = np.linspace(0,1,pts)
+    else:
+        th = np.linspace(0,1,n*20+1)
     a = np.cos(np.pi/n)
     b = np.cos(2*np.pi*((n*th)%1)/n-np.pi/n)
-    r = a/b
-    x = r*np.cos(2*np.pi*th)
-    y = r*np.sin(2*np.pi*th)
+    h = a/b
+    x = r*h*np.cos(2*np.pi*th)
+    y = r*h*np.sin(2*np.pi*th)
     
     return [(a,b) for a,b in zip(x,y)]
--- Geometry/ParametricPolygonExample.py
+++ Geometry/ParametricPolygonExample.py
@@ -3,4 +3,5 @@ from ParametricPolygon import parametric_polygon
 
 
 make_canvas([-3,3])
-scatter_points(parametric_polygon(4))
+scatter_points(parametric_polygon(7))
+scatter_points([[0,0]])
