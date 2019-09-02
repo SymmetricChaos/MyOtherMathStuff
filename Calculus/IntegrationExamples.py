e646fb51e9cf2621b765bb5c89ddc4e0ad2305be
SymmetricChaos <ajfraebel@gmail.com>
2019-09-02 13:35:11 -0400

    Integration

---
+++ Calculus/IntegrationExamples.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from IntegrationRules import rect_rule, trap_rule
+from Utils.Drawing import make_canvas
+from Geometry.Polygons import Polygon
+
+## Some weird function
+def func(x):
+    return x**4/15 + x**3/3 - 2*x**2 - 4*x + 10 + 8*np.sin(x)
+
+
+
+x = np.arange(-5,5,.1)
+y = func(x)
+
+make_canvas([-5,5],[-12,12])
+plt.plot(x,y)
+plt.fill_between(x,y,facecolor='lightgray')
+plt.plot([-5,5],[0,0],"k-")
+
+print("Area under the curve from -5 to 5")
+for i in [1,.5,.1]:
+    R = rect_rule(func,-5,5,i)
+    T = trap_rule(func,-5,5,i)
+    print("Step size {}".format(i))
+    print("By the rectangular rule {:.3f}".format(R))
+    print("By the trapezoidal rule {:.3f}".format(T))
+
+make_canvas([-6,6],[-13,13])
+plt.plot(x,y)
+plt.plot([-5,5],[0,0],"k-")
+a, b = -5, 5
+s = .5
+while a < b:
+    P = Polygon([[a,0],
+                 [a,func(a)],
+                 [a+s,func(a)],
+                 [a+s,0]
+                ])
+    P.draw(facecolor="#11111111")
+    a += s
+    
+make_canvas([-6,6],[-13,13])
+plt.plot(x,y)
+plt.plot([-5,5],[0,0],"k-")
+a, b = -5, 5
+s = 1
+while a < b:
+    P = Polygon([[a,0],
+                 [a,func(a)],
+                 [a+s,func(a+s)],
+                 [a+s,0]
+                ])
+    P.draw(facecolor="#11111111")
+    a += s
--- Calculus/IntegrationRules.py
+++ Calculus/IntegrationRules.py
@@ -1,7 +1,3 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-
 ## The rectangular rule
 def rect_rule(func,a,b,s):
     out = 0
@@ -18,24 +14,3 @@ def trap_rule(func,a,b,s):
         a += s
     return out
 
-## Some weird function
-def func(x):
-    return x**4/15 + x**3/3 - 2*x**2 - 4*x + 10 + 8*np.sin(x)
-
-
-
-x = np.arange(-5,5,.1)
-y = func(x)
-
-plt.plot(x,y)
-plt.fill_between(x,y,facecolor='lightgray')
-plt.plot([-5,5],[0,0],"k-")
-
-print("Area under the curve from -5 to 5")
-for i in [1,.5,.1]:
-    R = rect_rule(func,-5,5,i)
-    T = trap_rule(func,-5,5,i)
-    print("Step size {}".format(i))
-    print("By the rectangular rule {:.3f}".format(R))
-    print("By the trapezoidal rule {:.3f}".format(T))
-    
