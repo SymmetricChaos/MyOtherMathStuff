edc89907079606b02ebacfb984fa7fe703f29907
SymmetricChaos <ajfraebel@gmail.com>
2019-09-03 17:58:19 -0400

    Update LSystemTurtle.py

--- Fractals/LSystemTurtle.py
+++ Fractals/LSystemTurtle.py
@@ -8,7 +8,7 @@ def tree(S):
     else:
         return S
 
-for i in LSystem("B",tree,6):
+for i in LSystem("B",tree,7):
     rules = i
 
 
@@ -16,13 +16,13 @@ pos_stack = []
 ang_stack = []
 turtle.setheading(90)
 turtle.up()
-turtle.setpos(0,-200)
+turtle.setpos(0,-300)
 turtle.down()
 for i in rules:
     if i == "A":
-        turtle.forward(10)
+        turtle.forward(5)
     if i == "B":
-        turtle.forward(10)
+        turtle.forward(5)
     if i == "[":
         pos_stack.append(turtle.pos())
         ang_stack.append(turtle.heading())
@@ -36,5 +36,5 @@ for i in rules:
         turtle.down()
         turtle.right(45)
 turtle.up()
-turtle.setpos(0,-200)
+turtle.setpos(0,-300)
 turtle.done()
