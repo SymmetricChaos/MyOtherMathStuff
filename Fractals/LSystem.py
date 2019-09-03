5a1d5731005754bbfb65cb8ade21dcd377b87d11
SymmetricChaos <ajfraebel@gmail.com>
2019-09-02 22:05:42 -0400

    Update LSystem.py
    more examples and better function

--- Fractals/LSystem.py
+++ Fractals/LSystem.py
@@ -1,11 +1,13 @@
+## Context free L-systems
+
 def LSystem(state,rule,iters=3):
-    print(state)
+    yield state
     for i in range(iters):
         t = ""
         for l in state:
             t += rule(l)
-        print(t)
         state = t
+        yield state
         
 
 def func1(S):
@@ -13,10 +15,9 @@ def func1(S):
         return "AB"
     if S == "B":
         return "A"
-
-LSystem("A",func1)
-print()
-
+    else:
+        return S
+    
 def func2(S):
     if S == "A":
         return "AA"
@@ -24,5 +25,24 @@ def func2(S):
         return "A[B]B"
     else:
         return S
+    
+def func3(S):
+    if S == "|":
+        return "|_|"
+    if S == "_":
+        return "___"
+    else:
+        return S
 
-LSystem("B",func2)
+for i in LSystem("A",func1):
+    print(i)
+print()
+
+for i in LSystem("B",func2):
+    print(i)
+print()
+
+for i in LSystem("|",func3):
+    print(i)
+    print()
+print()
