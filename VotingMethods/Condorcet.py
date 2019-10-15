ddffc66eb884ef964ac583f135852922c617e389
SymmetricChaos <ajfraebel@gmail.com>
2019-10-15 11:50:57 -0400

    Create Condorcet.py

---
+++ VotingMethods/Condorcet.py
@@ -0,0 +1,56 @@
+# The Condorcet criterion requires that a voting method choose the candidate
+# that would win most head-to-head elections against other candidates. It only
+# works for ranked voting methods. Its not guaranteed that a Condorcet winner
+# exists and practical methods must deal with this.
+
+
+class Preferences:
+    
+    def __init__(self,candidates,ranks):
+        self.D = dict()
+        for c,r in zip(candidates,ranks):
+            self.D[str(c)] = r
+        self.longest_name = max( [len(i) for i in candidates] )
+
+
+    def __str__(self):
+        out = ""
+        for p in self.D.items():
+            out += f"{p[0]:{self.longest_name}}   {p[1]}\n"
+        return out
+
+
+    def __repr__(self):
+        return str(self.D)
+
+
+
+def compare_candidates(voters,candidates):
+    out = dict()
+    l = len(candidates)
+    for i in range(l):
+        for j in range(i+1,l):
+            wins = 0
+            for v in voters:
+                if v.D[candidates[i]] > v.D[candidates[j]]:
+                    wins += 1
+            out[f"{candidates[i]} vs {candidates[j]}"] = wins
+
+    print(out)
+
+
+if __name__ == '__main__':
+    import random
+    
+    candidates = ["Alice","Bob","Carol","Dave","Eve","Frank"]
+    
+    V = []
+    for i in range(5):
+    
+        ranks = [random.randint(0,5) for i in range(len(candidates))]
+        
+        V.append( Preferences(candidates,ranks) )
+        
+    for i in V:
+        print(i)
+    compare_candidates(V,candidates)
