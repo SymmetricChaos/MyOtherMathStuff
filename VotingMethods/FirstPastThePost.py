258ed07c7957818c1b7f4a1239c6dc5e20028a82
SymmetricChaos <ajfraebel@gmail.com>
2019-10-16 20:59:47 -0400

    Update FirstPastThePost.py

--- VotingMethods/FirstPastThePost.py
+++ VotingMethods/FirstPastThePost.py
@@ -2,8 +2,9 @@ from collections import Counter
 
 def FPTP(votes):
     C = Counter(votes)
-    for candidate in C.items():
-        print(candidate)
+    s = C.most_common()[0]
+    print(f"The Winner is {s[0]} with {s[1]} votes")
+    return C
     
 FPTP(["Alice","Bob","Alice","Carol","Bob","Bob","Alice","Bob"])
 
