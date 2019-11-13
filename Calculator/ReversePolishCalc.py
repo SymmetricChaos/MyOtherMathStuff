c306f6b031876c7193cbc7041ab95cda8b90f13f
SymmetricChaos <ajfraebel@gmail.com>
2019-11-13 07:54:37 -0500

    Update ReversePolishCalc.py

--- Calculator/ReversePolishCalc.py
+++ Calculator/ReversePolishCalc.py
@@ -1,26 +1,38 @@
-# Handle multiple arity
+from math import sqrt
+
+
 
 def reverse_polish(S):
     assert type(S) == str
     
-    ops = {"+" : lambda x,y: x+y,
-           "*" : lambda x,y: x*y,}
+    operation = {"+" : lambda x,y: x+y,
+                 "*" : lambda x,y: x*y,
+                 "sqrt" : lambda x: sqrt(x),
+                }
+    
+    arity = {"+" : 2,
+             "*" : 2,
+             "sqrt" : 1,
+             }
     
     L = S.split(" ")
 
     stk = []
     
     for i in L:
-        if i not in ops:
+        if i not in operation:
             stk.append(int(i))    
             print(stk)
-        else:    
+        else:
             print(stk,i)
-            a = stk.pop()
-            b = stk.pop()
-            r = ops[i](a,b)
+            args = []
+            for a in range(arity[i]):
+                args.append(stk.pop())
+            r = operation[i](*args)
             stk.append(r)
             print(stk)
+    
+    return stk
 
 
-reverse_polish("-2 8 3 73 + * +")
+reverse_polish("-2 8 3 73 + + + sqrt")
