b0d7def8c859501c8b9036323e3dacf16ce5585c
SymmetricChaos <ajfraebel@gmail.com>
2019-11-23 18:33:21 -0500

    Update LSystemHilbertCurveDraw.py

--- Fractals/LSystemHilbertCurveDraw.py
+++ Fractals/LSystemHilbertCurveDraw.py
@@ -10,38 +10,46 @@ def hilbert_curve_rule(S):
     else:
         return S
 
-for i in LSystem("B",hilbert_curve_rule,5):
-    rules = i
 
+def draw_hilbert(n):
 
-coords = [(0,0)]
+    for i in LSystem("B",hilbert_curve_rule,n):
+        rules = i
+    
+    coords = [(0,0)]
+    
+    # Strip out anything unnceesaary for drawing
+    rules = rules.replace("A","")
+    rules = rules.replace("B","")
+    rules = rules.replace("-+","")
+    rules = rules.replace("+-","")
+    while rules[0] == "+" or rules[0] == "-":
+        rules = rules[1:]
+        
+    ang = (n+1)%4
+    for i in rules:
+        if i == "F":
+            oldx = coords[-1][0] 
+            oldy = coords[-1][1]
+            if ang == 0:
+                coords.append( (oldx,oldy+1) )
+            elif ang == 1:
+                coords.append( (oldx+1,oldy) )
+            elif ang == 2:
+                coords.append( (oldx,oldy-1) )
+            elif ang == 3:
+                coords.append( (oldx-1,oldy) )
+        elif i == "-":
+            ang = (ang-1)%4
+        elif i == "+":
+            ang = (ang+1)%4
+    
+    xmax = max([i[0] for i in coords])
+    xmin = min([i[0] for i in coords])
+    make_canvas([xmin-1,xmax+1],size=4)
+    plot_points(coords)
 
-# Strip out anything unnceesaary for drawing
-rules = rules.replace("A","")
-rules = rules.replace("B","")
-rules = rules.replace("-+","")
-rules = rules.replace("+-","")
-rules = rules[1:]
-
-#print(rules)
-
-ang = 0
-for i in rules:
-    if i == "F":
-        oldx = coords[-1][0] 
-        oldy = coords[-1][1]
-        if ang == 0:
-            coords.append( (oldx,oldy+1) )
-        elif ang == 1:
-            coords.append( (oldx+1,oldy) )
-        elif ang == 2:
-            coords.append( (oldx,oldy-1) )
-        elif ang == 3:
-            coords.append( (oldx-1,oldy) )
-    elif i == "-":
-        ang = (ang-1)%4
-    elif i == "+":
-        ang = (ang+1)%4
-
-make_canvas([-40,40],size=6)
-plot_points(coords)
+draw_hilbert(2)
+draw_hilbert(3)
+draw_hilbert(4)
+draw_hilbert(5)
