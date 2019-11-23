b95d0c4dbe38b8968428d142870f593c357dc7da
SymmetricChaos <ajfraebel@gmail.com>
2019-11-23 09:36:23 -0500

    preferences method

--- VotingMethods/VoterTypes.py
+++ VotingMethods/VoterTypes.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-
 def sort_by_nth(L,n,func=None):
     if func == None:
         f = lambda x: x[n]
@@ -21,7 +19,8 @@ class Voter:
     
     def __init__(self,cands,ranks):
         self.prefs = prefs_to_dict(cands,ranks)
-    
+
+
     def __str__(self):
         out = ""
         for c,r in self.prefs.items():
@@ -29,15 +28,33 @@ class Voter:
         out = out[:-1]
         return out
 
+
     def rank(self,candidate):
         return self.prefs[candidate]
-    
+
+
     def favorite(self):
         return next(iter(self.prefs.keys()))
+
+
+    def prefers(self,A,B):
+        if self.rank(A) > self.rank(B):
+            return A
+        elif self.rank(A) < self.rank(B):
+            return B
+        else:
+            return None
 
 
 if __name__ == '__main__':
     V = Voter(["Alice","Bob","Carol","Dave","Eve","Frank"],[1,7,2,3,9,3])
     print(V)
+    print("\nFavorite")
     print(V.favorite())
+    print("\nRank for Bob")
     print(V.rank("Bob"))
+    
+    print("\nPreference of Alice vs Eve")
+    print(V.prefers("Alice","Eve"))
+    print("\nPreference of Frank vs Dave")
+    print(V.prefers("Frank","Dave"))
