4d3c86bb281290d40706a856d1a7907d01717fe1
SymmetricChaos <ajfraebel@gmail.com>
2019-12-05 01:01:53 -0500

    ahhhhhhhhhhh

--- CellularAutomata/GameOfLifeAnimFast.py
+++ CellularAutomata/GameOfLifeAnimFast.py
@@ -6,6 +6,7 @@ import matplotlib.animation as animation
 # Rather than track every point we track only the live points
 
 def filter_by_rule(D,cells):
+    print("filter step")
     newDict = defaultdict(int)
     for (key, val) in D.items():
         if key in cells:
@@ -14,10 +15,12 @@ def filter_by_rule(D,cells):
         else:
             if val == 3:
                 newDict[key] = 1
+    print(newDict)
     return newDict
 
 
 def add_neighbors(cell,grid):
+
     neighbors = [(-1,-1),(0,-1),(1,-1),
                  (-1,0) ,       (1,0),
                  (-1,1) ,(0,1), (1,1)]
@@ -26,20 +29,25 @@ def add_neighbors(cell,grid):
         grid[cell[0]+x,cell[0]+y] += 1
         
 def gen(grid):
-    live_cells = set(k for k,v in grid.items() if v == 1)
+    print("generation step")
+    live_cells = set(k for k,v in grid.items() if v != 0)
+    print("addition step")
     for c in live_cells:
         add_neighbors(c,grid)
 
-    return filter_by_rule(grid,live_cells)
+    grid = filter_by_rule(grid,live_cells)
+    print(grid)
+    print(id(grid))
+    return grid
 
 def coord_gen(grid):
-
+    print("coordinate step")
+    print(grid)
     x = []
     y = []
-    for k,v in grid.items():
-        if v == 1:
-            x.append(k[1])
-            y.append(k[0])
+    for k in grid:
+        x.append(k[1])
+        y.append(k[0])
 
     return x,y          
 
@@ -73,9 +81,12 @@ plt.ylim(-N//2-1, N//2)
 dots, = plt.plot([0], [0], 'ko', markersize=1)
 
 def anim_plot(num,grid,*args):
-    print(num)
+#    print(num)
+    print()
+    print("animation step")
     print(grid)
-    print(args)
+    print(id(grid))
+#    print(args)
     print()
     
     grid = gen(grid)
@@ -83,6 +94,6 @@ def anim_plot(num,grid,*args):
     dots.set_data(x,y)
     return dots
 
-life_anim = animation.FuncAnimation(fig, anim_plot, 20, fargs=(grid,0),
+life_anim = animation.FuncAnimation(fig, anim_plot, range(10), fargs=([grid]),
                                    interval=100, blit=False)
 life_anim.save('game_of_life_fast.mp4')
