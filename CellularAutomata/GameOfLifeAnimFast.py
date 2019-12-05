11eaaae6a594735ed57549e8532a126b969aa456
SymmetricChaos <ajfraebel@gmail.com>
2019-12-05 08:05:53 -0500

    it does the thing now

--- CellularAutomata/GameOfLifeAnimFast.py
+++ CellularAutomata/GameOfLifeAnimFast.py
@@ -6,7 +6,7 @@ import matplotlib.animation as animation
 # Rather than track every point we track only the live points
 
 def filter_by_rule(D,cells):
-    print("filter step")
+#    print("filter step")
     newDict = defaultdict(int)
     for (key, val) in D.items():
         if key in cells:
@@ -15,7 +15,6 @@ def filter_by_rule(D,cells):
         else:
             if val == 3:
                 newDict[key] = 1
-    print(newDict)
     return newDict
 
 
@@ -26,34 +25,32 @@ def add_neighbors(cell,grid):
                  (-1,1) ,(0,1), (1,1)]
     
     for x,y in neighbors:
-        grid[cell[0]+x,cell[0]+y] += 1
+        grid[cell[0]+x,cell[1]+y] += 1
+    
         
-def gen(grid):
-    print("generation step")
-    live_cells = set(k for k,v in grid.items() if v != 0)
-    print("addition step")
+def game_gen(grid):
+    live_cells = set(k for k,v in grid.items() if v == 1)
+
+    grid = defaultdict(int)
     for c in live_cells:
         add_neighbors(c,grid)
-
     grid = filter_by_rule(grid,live_cells)
-    print(grid)
-    print(id(grid))
+
     return grid
 
 def coord_gen(grid):
-    print("coordinate step")
-    print(grid)
+#    print("coordinate step")
     x = []
     y = []
-    for k in grid:
-        x.append(k[1])
-        y.append(k[0])
-
+    for k,v in grid.items():
+        if v == 1:
+            x.append(k[1])
+            y.append(k[0])
     return x,y          
 
 
 
-N = 50
+N = 200
 grid = defaultdict(int)
 
 grid[(0,0)] = 1
@@ -63,37 +60,27 @@ grid[(1,-1)] = 1
 grid[(-1,0)] = 1
 
 #print(grid)
-#grid = gen(grid)
-#x,y = coord_gen(grid)
-#print(grid)
-#grid = gen(grid)
-#x,y = coord_gen(grid)
-#print(grid)
-#grid = gen(grid)
-#x,y = coord_gen(grid)
+#grid = game_gen(grid)
 #print(grid)
 
 fig, ax = plt.subplots()
 fig.set_size_inches(6, 6)
 plt.xlim(-N//2-1, N//2)
 plt.ylim(-N//2-1, N//2)
+x,y = coord_gen(grid) 
+dots, = plt.plot(x, y, 'ko', markersize=.5)
 
-dots, = plt.plot([0], [0], 'ko', markersize=1)
-
-def anim_plot(num,grid,*args):
-#    print(num)
-    print()
-    print("animation step")
-    print(grid)
-    print(id(grid))
-#    print(args)
-    print()
+def anim_plot(num):
     
-    grid = gen(grid)
+    global grid 
     x,y = coord_gen(grid)
     dots.set_data(x,y)
-    return dots
 
-life_anim = animation.FuncAnimation(fig, anim_plot, range(10), fargs=([grid]),
+    grid = game_gen(grid)
+
+
+    return dots,
+
+life_anim = animation.FuncAnimation(fig, anim_plot, 1000,
                                    interval=100, blit=False)
 life_anim.save('game_of_life_fast.mp4')
