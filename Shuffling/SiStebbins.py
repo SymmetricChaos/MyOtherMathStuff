31a41de15c0b8d28317171b0fc4d503283475ece
SymmetricChaos <ajfraebel@gmail.com>
2020-01-14 19:04:27 -0500

    fixed

--- Shuffling/SiStebbins.py
+++ Shuffling/SiStebbins.py
@@ -1,13 +1,37 @@
+# Construct the Si Stebbins stack
+# The cards are in CHASED order and each card is of rank three greater
+# than the one before it
 rank = "A23456789TJQK"
-suit = "♠♥♣♦"
-D = []
+suit = "♣♥♠♦"
+si_deck = []
+
+pos1 = 0
+pos2 = 0
+for i in range(52):
+    si_deck.append(rank[pos1]+suit[pos2])
+    pos1 = (pos1 + 3) % 13
+    pos2 = (pos2 + 1) % 4
+    
+print(si_deck)
+print(len(set(si_deck)))
+
+
+
+# Alternative construction of a Si Stebbins type stack.
+
+# Create a deck of cards already sorted
+rank = "A23456789TJQK"
+suit = "♣♥♠♦"
+deck = []
 for s in suit:
     for r in rank:
-        D.append(r+s)
+        deck.append(r+s)
 
-si_stebbins = []
+si_deck = []
 pos = 0
 for i in range(52):
-    si_stebbins.append(D[pos])
-    pos = (pos + 17) % 52
-print(si_stebbins)
+    si_deck.append(deck[pos])
+    pos = (pos + 15) % 52
+    
+print(si_deck)
+print(len(set(si_deck)))
