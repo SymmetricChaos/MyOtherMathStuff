2be62e8eef7f15e5af5b059e87f1982ab169a82b
SymmetricChaos <ajfraebel@gmail.com>
2020-01-28 23:14:03 -0500

    trip the corpus

--- WordSearch/MakeCorpus.py
+++ WordSearch/MakeCorpus.py
@@ -6,8 +6,18 @@ C = []
 with open('Corpus.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
-        if len(row[0]) >= 3:
-            C += row
+        # Remove words too short to use
+        # Really even three letters is too short
+        # but they are good for kids word search
+        if len(row[0]) < 3:
+            continue
+        # Remove proper nouns
+        if any([x.isupper() for x in row[0]]):
+            continue
+
+        C += row
 
 print(C)
 print(len(C))
+
+pickle.dump(C, open( "Corpus.p", "wb" ) )
