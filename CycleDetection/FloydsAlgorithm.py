aef74b33bdc41337e72e87aa719dc4ed7b68367f
SymmetricChaos <ajfraebel@gmail.com>
2020-01-31 21:21:55 -0500

    Update FloydsAlgorithm.py

--- CycleDetection/FloydsAlgorithm.py
+++ CycleDetection/FloydsAlgorithm.py
@@ -8,7 +8,37 @@ def floyds_algorithm(func,x):
     while fast != slow:
         slow = func(slow)
         fast = func(func(fast))
+    
+    # Find the position where the cycle begins
+    pos = 0
+    slow = x
+    while fast != slow:
+        slow = func(slow)
+        fast = func(fast)
+        pos += 1
+    
+    # Find the length of the shortest cycle
+    length = 1
+    fast = f(slow)
+    while fast != slow:
+        fast = func(fast)
+        length += 1
+    
+    ex = x
+    for i in range(pos):
+        ex = func(ex)
+    
+    cyc = []
+    for i in range(length):
+        cyc.append(ex)
+        ex = func(ex)
 
+    return pos,length,cyc
+
+def iter_func(func,x,n):
+    for i in range(n):
+        x = func(x)
+    return x
 
 
 def f(n):
@@ -17,4 +47,7 @@ def f(n):
          8:0}
     return D[n]
 
-print(floyds_algorithm(f,0))
+for start in range(8):
+    print(f"Starting from {start}")
+    print(floyds_algorithm(f,start))
+    print()
