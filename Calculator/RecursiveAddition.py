36cdd0a48ca424182f3f082a8e41fec2e9a07183
SymmetricChaos <ajfraebel@gmail.com>
2020-02-11 18:57:35 -0500

    Update RecursiveAddition.py

--- Calculator/RecursiveAddition.py
+++ Calculator/RecursiveAddition.py
@@ -7,12 +7,12 @@ def pred(n):
     return n[2:-1]
 
 def recursive_addition(a,b):
+    print(a,b)
     if a == "0":
         return b
     elif b == "0":
         return a
     else:
-        print(a,b)
         return succ(recursive_addition(a,pred(b)))
 
 print(recursive_addition("S(S(0))","S(S(S(0)))"))
