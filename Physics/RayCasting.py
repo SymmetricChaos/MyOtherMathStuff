66fff9d02833093b05509dd82e78ed90ca0bba72
SymmetricChaos <ajfraebel@gmail.com>
2020-02-22 08:40:24 -0500

    Update RayCasting.py

--- Physics/RayCasting.py
+++ Physics/RayCasting.py
@@ -13,13 +13,26 @@ def distance(coord_a: tuple or list, coord_b: tuple or list):
     return math.hypot(coord_b[0] - coord_a[0], coord_b[1] - coord_a[1])
 
 
-def close_enough(coord_a: tuple, coord_b: tuple, distance: float):
+def is_close(coord_a: tuple, coord_b: tuple, mindist: float):
     """
     Calculate distance between two segment in 2D space and find if distance
     is less than minimum distance.
     :param coord_a: tuple -- (x, y) coords of first point
     :param coord_b: tuple -- (x, y) coords of second point
-    :param distance_: float -- minimal distance to check against
+    :param mindist: float -- minimal distance to check against
     :return: bool -- if distance is less than
     """
-    return distance(coord_a, coord_b) <= distance
+    return distance(coord_a, coord_b) <= mindist
+
+
+def polar_to_cart(angle: float, scalar: float):
+    """
+    Convert polar coordinates with cartesian coordinates
+    :param angle: float -- angle of the vector
+    :param scalar: float -- scalar value of the vector (e.g. speed)
+    :return: tuple -- x and y parts of the vector in format: (float, float)
+    """
+    radians = math.radians(angle)
+    change_y = math.cos(radians)
+    change_x = math.sin(radians)
+    return change_x * scalar, change_y * scalar
