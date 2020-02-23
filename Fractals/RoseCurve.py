cff1ac27d4d2c25d56e51151e6d634ad0f715fc4
SymmetricChaos <ajfraebel@gmail.com>
2020-02-23 18:25:55 -0500

    Update RoseCurve.py

--- Fractals/RoseCurve.py
+++ Fractals/RoseCurve.py
@@ -3,6 +3,8 @@ import numpy as np
 
 def simple_rose(k):
     
+    assert type(k) == int
+    
     if k % 2 == 0:
         pts = np.linspace(0,2*np.pi,1000)
     else:
@@ -14,13 +16,24 @@ def simple_rose(k):
     
     return x,y
 
-
+def rational_rose(n,d):
+    
+    assert type(n) == type(d) == int
+    pts = np.linspace(0,7*np.pi,1000)
+    
+    x = np.cos(pts*n/d)*np.cos(pts)
+    y = np.cos(pts*n/d)*np.sin(pts)
+    
+    return x,y
 
 if __name__ == '__main__':
-    x,y = simple_rose(3)
+#    x,y = simple_rose(3)
+    x,y = rational_rose(3,7)
     
     fig = plt.figure()
     fig.set_size_inches(10,10)
     plt.axes().set_aspect("equal","datalim")
     plt.axis("off")
     plt.plot(x,y)
+    
+    
