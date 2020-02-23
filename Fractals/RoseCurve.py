72558faae7451eecc001c29e65d0f8bedddc9ba8
SymmetricChaos <ajfraebel@gmail.com>
2020-02-23 18:20:07 -0500

    Update RoseCurve.py

--- Fractals/RoseCurve.py
+++ Fractals/RoseCurve.py
@@ -1,19 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def rose_path(k):
-    pts = np.linspace(0,2*np.pi,1000)
-    pts_spaced = pts*k
+def simple_rose(k):
     
-    x = np.cos(pts_spaced)*np.cos(pts)
-    y = np.cos(pts_spaced)*np.sin(pts)
+    if k % 2 == 0:
+        pts = np.linspace(0,2*np.pi,1000)
+    else:
+        pts = np.linspace(0,np.pi,1000)
+    
+    
+    x = np.cos(pts*k)*np.cos(pts)
+    y = np.cos(pts*k)*np.sin(pts)
     
     return x,y
-    
-x,y = rose_path(7)
 
-fig = plt.figure()
-fig.set_size_inches(10,10)
-plt.axes().set_aspect("equal","datalim")
-plt.axis("off")
-plt.plot(x,y)
+
+
+if __name__ == '__main__':
+    x,y = simple_rose(3)
+    
+    fig = plt.figure()
+    fig.set_size_inches(10,10)
+    plt.axes().set_aspect("equal","datalim")
+    plt.axis("off")
+    plt.plot(x,y)
