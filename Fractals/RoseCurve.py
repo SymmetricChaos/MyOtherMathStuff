c06fb307b940681326da4162ad96ab9604412dd7
SymmetricChaos <ajfraebel@gmail.com>
2020-02-24 20:59:03 -0500

    Update RoseCurve.py

--- Fractals/RoseCurve.py
+++ Fractals/RoseCurve.py
@@ -1,39 +1,32 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def simple_rose(k):
-    
-    assert type(k) == int
-    
-    if k % 2 == 0:
-        pts = np.linspace(0,2*np.pi,1000)
-    else:
-        pts = np.linspace(0,np.pi,1000)
-    
-    
-    x = np.cos(pts*k)*np.cos(pts)
-    y = np.cos(pts*k)*np.sin(pts)
-    
-    return x,y
+def polar_to_cart(r,th):
+    return r*np.cos(th), r*np.sin(th)
 
-def rational_rose(n,d):
-    
-    assert type(n) == type(d) == int
-    pts = np.linspace(0,7*np.pi,1000)
-    
-    x = np.cos(pts*n/d)*np.cos(pts)
-    y = np.cos(pts*n/d)*np.sin(pts)
-    
-    return x,y
 
-if __name__ == '__main__':
-#    x,y = simple_rose(3)
-    x,y = rational_rose(3,7)
+def rose(n,d,c=0,m=2):
     
+    assert type(n) == type(d) == type(m) == int
+    
+    th = np.linspace(0,m*np.pi,1000)
+    
+    r = np.cos(n/d*th)+c
+    
+    x,y = polar_to_cart(r,th)
+
     fig = plt.figure()
     fig.set_size_inches(10,10)
     plt.axes().set_aspect("equal","datalim")
     plt.axis("off")
     plt.plot(x,y)
+    plt.title(f"Rose(n={n}, d={d}, c={c})",fontsize=25)
     
+    return x,y
     
+
+
+
+
+if __name__ == '__main__':
+    x,y = rose(5,6,0,12)
