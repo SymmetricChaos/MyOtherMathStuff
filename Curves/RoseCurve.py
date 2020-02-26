97ea0615e40235514780ca1c03bc5e05f4e3caad
SymmetricChaos <ajfraebel@gmail.com>
2020-02-25 19:28:33 -0500

    better rose plotting

--- Curves/RoseCurve.py
+++ Curves/RoseCurve.py
@@ -5,28 +5,55 @@ def polar_to_cart(r,th):
     return r*np.cos(th), r*np.sin(th)
 
 
-def rose(n,d,c=0,m=2):
+def rose(n,d=1,c=0,k=1000,draw=True):
     
-    assert type(n) == type(d) == type(m) == int
+    assert type(n) == type(d) == type(k) == int
     
-    th = np.linspace(0,m*np.pi,1000)
+    if n%2 == 0 or d%2 == 0:
+        th = np.linspace(0,2*d*np.pi,k)
+    else:
+        th = np.linspace(0,d*np.pi,k)
     
-    r = np.cos(n/d*th)+c#+np.sin(th*10)/20
+    r = np.cos(n/d*th)+c
     
     x,y = polar_to_cart(r,th)
 
-    fig = plt.figure()
-    fig.set_size_inches(10,10)
-    plt.axes().set_aspect("equal","datalim")
-    plt.axis("off")
-    plt.plot(x,y)
-    plt.title(f"Rose(n={n}, d={d}, c={c})",fontsize=25)
-    
+    if draw == True:
+        fig = plt.figure()
+        fig.set_size_inches(10,10)
+        plt.axes().set_aspect("equal","datalim")
+        plt.axis("off")
+        plt.plot(x,y)
+        plt.title(f"Rose(n={n}, d={d}, c={c})",fontsize=25)
+        
     return x,y
-    
 
+
+#def maurer_rose(n,m=361,ang=10,draw=True):
+#    
+#    assert type(n) == type(d) == type(m) == int
+#    
+#    degrees = ang*np.pi/180
+#    print(ang)
+#    print(degrees)
+#    angles = np.array([degrees*x for x in range(m)])
+#    print(angles%(2*np.pi))
+#    radii = np.sin(n*angles)
+#    
+#    x,y = polar_to_cart(radii,angles)
+#
+#    if draw == True:
+#        fig = plt.figure()
+#        fig.set_size_inches(10,10)
+#        plt.axes().set_aspect("equal","datalim")
+#        plt.axis("off")
+#        plt.plot(x,y)
+#        plt.title(f"MaurerRose(n={n}, d={d}, c={c})",fontsize=25)
+#        
+#    return x,y
 
 
 
 if __name__ == '__main__':
-    x,y = rose(5,6,1.5,12)
+    x,y = rose(4,7,1)
+#    maurer_rose(2)
