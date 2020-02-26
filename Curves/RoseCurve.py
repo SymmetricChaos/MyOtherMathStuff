8cf2cc9ca952a423bd3af61dc081c37d27d83048
SymmetricChaos <ajfraebel@gmail.com>
2020-02-25 19:49:36 -0500

    Correct mauer rose

--- Curves/RoseCurve.py
+++ Curves/RoseCurve.py
@@ -24,36 +24,36 @@ def rose(n,d=1,c=0,k=1000,draw=True):
         plt.axes().set_aspect("equal","datalim")
         plt.axis("off")
         plt.plot(x,y)
-        plt.title(f"Rose(n={n}, d={d}, c={c})",fontsize=25)
+        plt.title(f"Rose(k={n}/{d}, c={c})",fontsize=25)
         
     return x,y
 
 
-#def maurer_rose(n,m=361,ang=10,draw=True):
-#    
-#    assert type(n) == type(d) == type(m) == int
-#    
-#    degrees = ang*np.pi/180
-#    print(ang)
-#    print(degrees)
-#    angles = np.array([degrees*x for x in range(m)])
-#    print(angles%(2*np.pi))
-#    radii = np.sin(n*angles)
-#    
-#    x,y = polar_to_cart(radii,angles)
-#
-#    if draw == True:
-#        fig = plt.figure()
-#        fig.set_size_inches(10,10)
-#        plt.axes().set_aspect("equal","datalim")
-#        plt.axis("off")
-#        plt.plot(x,y)
-#        plt.title(f"MaurerRose(n={n}, d={d}, c={c})",fontsize=25)
-#        
-#    return x,y
+def maurer_rose(n,d=1,c=0,m=361,deg=10,draw=True):
+    
+    rad = deg*np.pi/180
+    
+    angs = np.array([rad*k for k in range(m)])
+    
+    radii = np.cos(n/d*angs)+c
+    
+    x,y = polar_to_cart(radii,angs)
+    
+    xr, yr = rose(n,d,c,draw=False)
+
+    if draw == True:
+        fig = plt.figure()
+        fig.set_size_inches(10,10)
+        plt.axes().set_aspect("equal","datalim")
+        plt.axis("off")
+        plt.plot(x,y)
+        plt.plot(xr,yr)
+        plt.title(f"MaurerRose(k={n}/{d},m={m},d={deg})",fontsize=25)
+        
+    return x,y
 
 
 
 if __name__ == '__main__':
     x,y = rose(4,7,1)
-#    maurer_rose(2)
+    maurer_rose(6,d=5,c=1,m=1061,deg=71)
