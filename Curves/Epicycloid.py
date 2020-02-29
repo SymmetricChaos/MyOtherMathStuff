fcfe3b4f458e00cdd1d39a0a2c1ea86e09b7e9bb
SymmetricChaos <ajfraebel@gmail.com>
2020-02-29 11:14:53 -0500

    Aesthetics

--- Curves/Epicycloid.py
+++ Curves/Epicycloid.py
@@ -2,34 +2,35 @@ import matplotlib.pyplot as plt
 import numpy as np
 from GCD import gcd
 
-def epicycloid(n,d,draw=True):
+def epicycloid(incirc,outcirc,draw=True):
     
-    g = gcd(n,d)
-    n, d = n//g, d//g
-    k = n/d
+    R, r = incirc, outcirc
+    g = gcd(R,r)
+    R, r = R//g, r//g
+    s = r+R
     
-    th = np.linspace(0,n*d*np.pi,1000)
+    th = np.linspace(0,R*r*np.pi,1000)
     
-    x = (k+1)*np.cos(th) - np.cos((k+1)*th)
-    y = (k+1)*np.sin(th) - np.sin((k+1)*th)
+    x = s*np.cos(th) - r*np.cos(s/r*th)
+    y = s*np.sin(th) - r*np.sin(s/r*th)
     
-    x_circ = np.cos(th)*k
-    y_circ = np.sin(th)*k
+    th = np.linspace(0,2*np.pi,1000)
+    x_circ = np.cos(th)*R
+    y_circ = np.sin(th)*R
     
     if draw == True:
         fig = plt.figure()
         fig.set_size_inches(10,10)
         plt.axes().set_aspect("equal","datalim")
         plt.axis("off")
-        plt.plot(x,y)
-        plt.plot(x_circ,y_circ)
-        if d == 1:
-            plt.title(f"Epicycloid(k={n})",fontsize=25)
-        else:
-            plt.title(f"Epicycloid(k={n}/{d})",fontsize=25)
+        plt.plot(x_circ,y_circ,color="black")
+        plt.plot(x,y,color="CornflowerBlue")
+        plt.title(f"Epicycloid(R={R},r={r})",fontsize=25)
 
     return x,y
 
 
-x,y = epicycloid(5,1)
-x,y = epicycloid(4,3)
+x,y = epicycloid(3,1)
+x,y = epicycloid(1,2)
+x,y = epicycloid(3,2)
+x,y = epicycloid(2,3)
