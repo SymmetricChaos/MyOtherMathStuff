178aa41f44c80e9d5e6994b8c0d9ce370ebc24a4
SymmetricChaos <ajfraebel@gmail.com>
2020-03-04 21:04:58 -0500

    Update Spirals.py

--- Curves/Spirals.py
+++ Curves/Spirals.py
@@ -2,13 +2,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 from SimpleCurves import polar_to_cart
 
-def archimedian_spiral(a=0,b=1,turns=1,n=1001):
+def archimedian_spiral(a=0,b=1,turns=1,draw=True,n=1001):
     th = np.linspace(0,2*turns*np.pi,n)
     
     r = a+(b*th)
     
-    return polar_to_cart(r,th)
+    x,y = polar_to_cart(r,th)
+    
+    if draw == True:
+        fig = plt.figure()
+        fig.set_size_inches(10,10)
+        plt.axes().set_aspect("equal","datalim")
+        plt.axis("off")
+        
+        plt.plot(x,y,color="CornflowerBlue",linewidth=2)
 
-x,y = archimedian_spiral(turns=2)
+    
+    return x,y
 
-plt.plot(x,y)
+def hyperbolic_spiral(a=0,b=1,turns=1,draw=True,n=1001):
+    th = np.linspace(0,2*turns*np.pi,n)
+    
+    r = a+(b/th)
+    
+    x,y = polar_to_cart(r,th)
+    
+    if draw == True:
+        fig = plt.figure()
+        fig.set_size_inches(10,10)
+        plt.axes().set_aspect("equal","datalim")
+        plt.axis("off")
+        
+        plt.plot(x,y,color="CornflowerBlue",linewidth=2)
+
+    
+    return x,y
+
+
+if __name__ == '__main__':
+    
+    x,y = archimedian_spiral(10,.1,turns=5)
+#    x,y = hyperbolic_spiral(turns=2,n=101)
