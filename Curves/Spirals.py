53035957d6eee1a4aff49adccefbb5d1ec6479e6
SymmetricChaos <ajfraebel@gmail.com>
2020-03-07 08:06:34 -0500

    Update Spirals.py

--- Curves/Spirals.py
+++ Curves/Spirals.py
@@ -2,12 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from SimpleCurves import polar_to_cart
 
-def draw_spiral(x,y):
+def draw_spiral(x,y,title=""):
     fig = plt.figure()
     fig.set_size_inches(10,10)
     plt.axes().set_aspect("equal","datalim")
     plt.axis("off")
-        
+    plt.title(title,size=20)
     plt.plot(x,y,color="CornflowerBlue",linewidth=2)
 
 
@@ -22,11 +22,9 @@ def archimedian_spiral(a=1,turns=1,bounded=False,draw=True,n=1001):
     x,y = polar_to_cart(r,th)
     
     if draw == True:
-        draw_spiral(x,y)
+        draw_spiral(x,y,f"Archimedian Spiral with {turns} turns")
         
     return x,y
-
-
 
 
 def hyperbolic_spiral(a=1,turns=1,draw=True,n=1001):
@@ -37,7 +35,7 @@ def hyperbolic_spiral(a=1,turns=1,draw=True,n=1001):
     x,y = polar_to_cart(r,th)
     
     if draw == True:
-        draw_spiral(x,y)
+        draw_spiral(x,y,f"Hyperbolic Spiral with {turns} turns")
     return x,y
 
 
@@ -72,7 +70,7 @@ def logarithmic_spiral(a=1,e=np.e,k=1,turns=1,bounded=False,draw=True,n=1001):
     x,y = polar_to_cart(r,th)
     
     if draw == True:
-        draw_spiral(x,y)
+        draw_spiral(x,y,f"Logarithmic Spiral with {turns} turns")
     
     return x,y
 
@@ -83,6 +81,7 @@ def logarithmic_spiral(a=1,e=np.e,k=1,turns=1,bounded=False,draw=True,n=1001):
 if __name__ == '__main__':
     
     x,y = archimedian_spiral(a=.1,turns=5)
+    x,y = archimedian_spiral(a=.1,turns=5,bounded=True)
     x,y = hyperbolic_spiral(turns=200,n=5001)
     x1,y2,x2,y2 = fermat_spiral(5,turns=5)
     x,y = logarithmic_spiral(e=1.2,turns=5)
