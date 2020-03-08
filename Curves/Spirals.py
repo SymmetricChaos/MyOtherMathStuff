945c28559a93c2f52cc18e003917985020064543
SymmetricChaos <ajfraebel@gmail.com>
2020-03-07 21:25:35 -0500

    option for multiple branches

--- Curves/Spirals.py
+++ Curves/Spirals.py
@@ -11,7 +11,7 @@ def draw_spiral(x,y,title=""):
     plt.plot(x,y,color="CornflowerBlue",linewidth=2)
 
 
-def archimedian_spiral(a=1,turns=1,bounded=False,draw=True,n=1001):
+def archimedian_spiral(a=1,turns=1,both_branches=False,bounded=False,draw=True,n=1001):
     th = np.linspace(0,2*turns*np.pi,n)
     
     if bounded:
@@ -20,6 +20,10 @@ def archimedian_spiral(a=1,turns=1,bounded=False,draw=True,n=1001):
         r = a*th
     
     x,y = polar_to_cart(r,th)
+    
+    if both_branches:
+        x = np.append(np.flip(x),-x)
+        y = np.append(np.flip(y),-y)
     
     if draw == True:
         if bounded:
@@ -45,15 +49,16 @@ def hyperbolic_spiral(a=1,turns=1,draw=True,n=1001):
     return x,y
 
 
-def fermat_spiral(a=1,k=.5,turns=1,draw=True,n=1001):
+def fermat_spiral(a=1,k=.5,turns=1,both_branches=True,draw=True,n=1001):
     th = np.linspace(0,2*turns*np.pi,n)
     
     r = a*th**k
     
     x,y = polar_to_cart(r,th)
     
-    x = np.append(np.flip(x),-x)
-    y = np.append(np.flip(y),-y)
+    if both_branches:
+        x = np.append(np.flip(x),-x)
+        y = np.append(np.flip(y),-y)
     
     if draw == True:
         draw_spiral(x,y,f"Fermat Spiral with {turns} turns")
@@ -62,15 +67,16 @@ def fermat_spiral(a=1,k=.5,turns=1,draw=True,n=1001):
     return x,y
 
 
-def logarithmic_spiral(a=1,e=np.e,k=1,turns=1,bounded=False,draw=True,n=1001):
+def logarithmic_spiral(a=1,e=np.e,k=1,turns=1,both_branches=False,draw=True,n=1001):
     th = np.linspace(0,2*turns*np.pi,n)
     
-    if bounded:
-        r = np.arctan(a*e**(k*th))
-    else:
-        r = a*e**(k*th)
+    r = a*e**(k*th)
         
     x,y = polar_to_cart(r,th)
+    
+    if both_branches:
+        x = np.append(np.flip(x),-x)
+        y = np.append(np.flip(y),-y)
     
     if draw == True:
         draw_spiral(x,y,f"Logarithmic Spiral with {turns} turns")
@@ -83,8 +89,10 @@ def logarithmic_spiral(a=1,e=np.e,k=1,turns=1,bounded=False,draw=True,n=1001):
 
 if __name__ == '__main__':
     
-#    x,y = archimedian_spiral(a=.1,turns=5)
+    archimedian_spiral(a=.1,turns=5)
+    archimedian_spiral(a=.1,turns=5,both_branches=True)
+    logarithmic_spiral(e=1.2,turns=5)
+    logarithmic_spiral(e=1.2,turns=5,both_branches=True)
 #    x,y = archimedian_spiral(a=.1,turns=5,bounded=True)
 #    x,y = hyperbolic_spiral(.1,turns=10,n=5001)
-    x,y = fermat_spiral(1,k=.5,turns=10,n=1001)
-#    x,y = logarithmic_spiral(e=1.2,turns=5)
+#    x,y = fermat_spiral(1,k=.5,turns=10,n=1001)
