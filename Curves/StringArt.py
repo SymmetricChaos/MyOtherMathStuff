bc19aa548a10a872dfb5723c88141df0d8f2fe8b
SymmetricChaos <ajfraebel@gmail.com>
2020-03-10 19:08:24 -0400

    Update StringArt.py

--- Curves/StringArt.py
+++ Curves/StringArt.py
@@ -7,12 +7,12 @@ def interpolation(A,B,p):
     y = A[1]*(1-p) + B[1]*p
     return [x,y]
 
-def string_bezier2(P1=[0,1],P2=[1,0],n=15,size=5):
+def string_bezier2(P1=[0,1],P2=[1,0],n=15,size=5,colormap="plasma"):
     
     t = np.linspace(0,1,n)
     X0,Y0 = interpolation(P1,[0,0],t)
     X1,Y1 = interpolation([0,0],P2,t)
-    
+
     
     ax = plt.axes()
     ax.set_aspect("equal","datalim")
@@ -20,7 +20,9 @@ def string_bezier2(P1=[0,1],P2=[1,0],n=15,size=5):
     ax.set_xticks([])
     ax.set_yticks([])
     
-    for x0,y0,x1,y1 in zip(X0,Y0,X1,Y1):
-        plt.plot([x0,x1],[y0,y1])
+    for x0,y0,x1,y1,color_pt in zip(X0,Y0,X1,Y1,t):
+        plt.plot([x0,x1],[y0,y1],color=plt.get_cmap(colormap)(color_pt))
 
-string_bezier2([1,1],n=20)
+
+if __name__ == '__main__':
+    string_bezier2([.5,1],n=35,colormap="coolwarm")
