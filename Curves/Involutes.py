3bfcd973ad57f5b18b021b16bbceee9beac66c7f
SymmetricChaos <ajfraebel@gmail.com>
2020-03-13 18:44:05 -0400

    Update Involutes.py

--- Curves/Involutes.py
+++ Curves/Involutes.py
@@ -1,22 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def circle_involute(a=0,r=1,turns=1,n=1001):
+def circle_involute(a=0,r=1,turns=1,n=1001,draw=True):
     
-    th = np.linspace(0,2*turns*np.pi,n)
+    th = np.linspace(a,2*turns*np.pi+a,n)
     x = r*(np.cos(th)+(th-a)*np.sin(th))
     y = r*(np.sin(th)-(th-a)*np.cos(th))
 
-    fig = plt.figure()
-    fig.set_size_inches(7,7)
-    ax = plt.axes()
-    ax.set_aspect("equal","datalim")
-    ax.axis('off')
-    ax.set_xticks([])
-    ax.set_yticks([])
-    
-    plt.plot(x,y)
+    if draw:
+        fig = plt.figure()
+        fig.set_size_inches(7,7)
+        ax = plt.axes()
+        ax.set_aspect("equal","datalim")
+        ax.axis('off')
+        ax.set_xticks([])
+        ax.set_yticks([])
+        
+        plt.plot(x,y)
+        
+    return x,y
+
 
 
 if __name__ == '__main__':
-    circle_involute(a=0,turns=2)
+    x1,y1 = circle_involute(a=0,turns=1)
+    x2,y2 = circle_involute(a=2,turns=1)
+    x3,y3 = circle_involute(a=4,turns=1)
+    
+    th = np.linspace(0,2*np.pi,101)
+    x,y = np.sin(th), np.cos(th)
+    plt.plot(x,y)
+    plt.plot(x1,y1)
+    plt.plot(x2,y2)
+    plt.plot(x3,y3)
+    
