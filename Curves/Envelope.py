dd13a2601bf463d11e9fc9408c03c7ab247fe2db
SymmetricChaos <ajfraebel@gmail.com>
2020-03-17 08:20:35 -0400

    fixed it
    not sure how I got those flipped, did the math right in notepad and never noticed

--- Curves/Envelope.py
+++ Curves/Envelope.py
@@ -1,34 +1,37 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def envelope(slopes,intercepts):
+def envelope(slopes,intercepts,draw=True):
 
     shifted_slopes = np.roll(slopes,1)
     shifted_intercepts = np.roll(intercepts,1)
     
-    print(slopes[:10])
-    print(shifted_slopes[:10])
-    
-    x = (slopes-shifted_slopes)/(shifted_intercepts-intercepts)
+    x = (shifted_intercepts-intercepts)/(slopes-shifted_slopes)
     y = slopes*x+intercepts
     
-#    fig = plt.figure()
-#    fig.set_size_inches(12,12)
-#    ax = plt.axes(xlim=(-5,5), ylim=(-5,5))
-#    ax.axis('off')
-#    ax.set_xticks([])
-#    ax.set_yticks([])
-#    
-#    plt.plot(x,y)
+    if draw == True:
+        fig = plt.figure()
+        fig.set_size_inches(12,12)
+        ax = plt.axes(xlim=(-5,5), ylim=(-5,5))
+        ax.axis('off')
+        ax.set_xticks([])
+        ax.set_yticks([])
+        
+    plt.plot(x,y)
     return x,y
-        
-        
+
+
+
+
+
 if __name__ == '__main__':
     from Normals import normals
+    from Spirogram import trochoid
     from SimpleCurves import ellipse
     
-    x,y = ellipse(1,2,81)
-    m,b = normals(x,y,draw=True)
-    x1,y1 = envelope(m,b)
+    x,y = trochoid(4,1,2,hypo=True,n=2001,draw=False)
+    m,b = normals(x,y,size=8,color="salmon",alpha=.03)
     plt.plot(x,y)
-    plt.scatter(x1,y1)
+    
+    x1,y1 = envelope(m,b,draw=False)
+    plt.plot(x1,y1)
