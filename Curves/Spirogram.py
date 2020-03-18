3fee338dfa17d5aa93d0cddf381545966a59a5b2
SymmetricChaos <ajfraebel@gmail.com>
2020-03-18 18:28:35 -0400

    Update Spirogram.py

--- Curves/Spirogram.py
+++ Curves/Spirogram.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from GCD import gcd
 
-def trochoid(incirc,outcirc,d=0,hypo=False,draw=True,n=2000):
+def trochoid(incirc,outcirc,d=0,hypo=False,draw=True,n=2001):
     
     # Rename incirc and outcirc
     R, r = incirc, outcirc
@@ -18,18 +18,41 @@ def trochoid(incirc,outcirc,d=0,hypo=False,draw=True,n=2000):
     
     # Calculate the roulette for either an epitrochoid or a hypotrochoid
     if hypo == False:
-        s = r+R
+        s = R+r
         x = s*np.cos(th) - d*np.cos(s/r*th)
         y = s*np.sin(th) - d*np.sin(s/r*th)
     else:
         s = R-r
         x = s*np.cos(th) + d*np.cos(s/r*th)
         y = s*np.sin(th) - d*np.sin(s/r*th)
+
+    # Draw everything
+    if draw == True:
+        fig = plt.figure()
+        fig.set_size_inches(10,10)
+        plt.axes().set_aspect("equal","datalim")
+        plt.axis("off")
+        
+        plt.plot(x,y,color="CornflowerBlue",linewidth=2)
+
+    return x,y
+
+def trochoid_explanation(incirc,outcirc,d=0,hypo=False,draw=True,n=2001):
+    
+    # Rename incirc and outcirc
+    R, r = incirc, outcirc
+    
+    x,y = trochoid(R,r,d,hypo,False,n)
     
     # Points of the inner circle
-    th = np.linspace(0,2*np.pi,1000)
+    th = np.linspace(0,2*np.pi,1001)
     x_circ1 = np.cos(th)*R
     y_circ1 = np.sin(th)*R
+    
+    if hypo == False:
+        s = R+r
+    else:
+        s = R-r
     
     # Points of the outer circle position for either an epitrochoid or a hypotrochoid
     if hypo == False:
@@ -60,11 +83,11 @@ def trochoid(incirc,outcirc,d=0,hypo=False,draw=True,n=2000):
         plt.plot(x_circ2,y_circ2,color="black",zorder=10,linewidth=3)
         plt.scatter(start_point,0,color="black",zorder=10)
         plt.plot([start_point,center_point],[0,0],color="black",linewidth=3)
-
-    return x,y
+        
+        
 
 if __name__ == '__main__':
-    x,y = trochoid(4,5,7)
-    x,y = trochoid(3,4,2)
-    x,y = trochoid(5,2,6,hypo=True)
-    x,y = trochoid(5,2,1,hypo=True)
+    trochoid_explanation(4,5,7)
+    trochoid_explanation(3,4,2)
+    trochoid_explanation(5,2,6,hypo=True)
+    trochoid_explanation(5,2,1,hypo=True)
