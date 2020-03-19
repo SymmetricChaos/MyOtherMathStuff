afdf14c15a9c448294ad12f84112ece978efe45f
SymmetricChaos <ajfraebel@gmail.com>
2020-03-19 12:42:00 -0400

    draw lines from slope and intercept

---
+++ Curves/mbline.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+def mbline(M,B,xlim=[-5,5],color="CornflowerBlue",alpha=1):
+    
+    
+    for m,b in zip(M,B):
+        
+        x0 = xlim[0]
+        y0 = m*x0+b
+        
+        x1 = xlim[1]
+        y1 = m*x1+b
+
+        plt.plot([x0,x1],[y0,y1],color=color,alpha=alpha)
