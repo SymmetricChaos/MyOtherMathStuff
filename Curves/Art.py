aade8926e616cccf12c9e6dd6d7515beeb760ca5
SymmetricChaos <ajfraebel@gmail.com>
2020-03-24 16:55:22 -0400

    Update Art.py

--- Curves/Art.py
+++ Curves/Art.py
@@ -7,19 +7,19 @@ from Tangents import tangents
 from SimpleCurves import ellipse
 
 def double_ellipse():
-    x,y = ellipse(1,2)
+    x,y = ellipse(1,2,501)
     m1,b1 = normals(x,y,draw=False)
     m2,b2 = tangents(x,y,draw=False)
     
     fig = plt.figure()
-    fig.set_size_inches(12,12)
-    ax = plt.axes(xlim=(-6,6), ylim=(-6,6))
+    fig.set_size_inches(14,14)
+    ax = plt.axes(xlim=(-4.5,4.5), ylim=(-4.5,4.5))
     ax.axis('off')
     ax.set_xticks([])
     ax.set_yticks([])
     
-    mbline(m1[1:],b1[1:],alpha=.02,color='red')
-    mbline(m2,b2,alpha=.02,color='blue')
+    mbline(m1,b1,alpha=.05,color='red')
+    mbline(m2[1:],b2[1:],alpha=.05,color='red')
     
     
 
