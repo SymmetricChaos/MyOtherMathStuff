f8b87bfe65b20bc33c71ab931f73d72006a4c372
SymmetricChaos <ajfraebel@gmail.com>
2020-03-24 17:32:16 -0400

    moved bezier example, improved demonstration, also integer coords

--- Curves/Art.py
+++ Curves/Art.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.spatial import ConvexHull
 from mbline import mbline
 from Spirogram import trochoid
 from Normals import normals
 from Tangents import tangents
 from SimpleCurves import ellipse
+from Bezier import bezier
 
 def double_ellipse():
     x,y = ellipse(1,2,501)
@@ -22,6 +24,45 @@ def double_ellipse():
     mbline(m2[1:],b2[1:],alpha=.05,color='red')
     
     
+def bezier_example():
+    
+    points = np.array([ (-4,0), 
+                        (2,-4),
+                        (3,-2),
+                        (-2,4), 
+                        (-1,2), 
+                        (2,0) ])
+
+    xs = [p[0] for p in points]
+    ys = [p[1] for p in points]
+    
+    X,Y = bezier(points)
+    
+    # Plot the curve, the points, and annotations
+    fig = plt.figure()
+    fig.set_size_inches(12,12)
+    ax = plt.axes()
+    ax.set_aspect("equal","datalim")
+    ax.axis('off')
+    ax.set_xticks([])
+    ax.set_yticks([])
+    
+    plt.plot(X,Y)
+    plt.scatter(xs,ys)
+    plt.title(f"Bezier Curve with {len(points)} Control Points\nBounding Property of Convex Hull Shown\n",size=20)
+    for num,pos in enumerate(points):    
+        plt.annotate(f"{num+1}: ({pos[0]},{pos[1]})",[pos[0],pos[1]+.1],size=14)
+
+
+    # Show convex hull property
+    hull = ConvexHull(points)
+    for simplex in hull.simplices:
+         plt.plot(points[simplex, 0], points[simplex, 1], 'lightgray',zorder=-1)
+
+
+
+    
 
 if __name__ == "__main__":
-    double_ellipse()
+#    double_ellipse()
+    bezier_example()
--- Curves/Bezier.py
+++ Curves/Bezier.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.spatial import ConvexHull
  
 def interpolation(A,B,p):
     """The point that is some exactly p percent of the way between A and B"""
@@ -27,36 +26,6 @@ def bezier(control_points,N=50):
 
         L = P.copy()
 
-
-def bezier_example(points,N=50):
-    
-    points = np.array(points)
-
-    xs = [p[0] for p in points]
-    ys = [p[1] for p in points]
-    
-    X,Y = bezier(points)
-    
-    # Plot the curve, the points, and annotations
-    fig = plt.figure()
-    fig.set_size_inches(8,8)
-    ax = plt.axes()
-    ax.set_aspect("equal","datalim")
-    ax.axis('off')
-    ax.set_xticks([])
-    ax.set_yticks([])
-    
-    plt.plot(X,Y)
-    plt.scatter(xs,ys)
-    plt.title(f"Bezier Curve with {len(points)} Control Points",size=20)
-    for num,pos in enumerate(points):    
-        plt.annotate(f"{num}: {pos}",[pos[0],pos[1]+.1])
-
-
-    # Show convex hull property
-    hull = ConvexHull(points)
-    for simplex in hull.simplices:
-         plt.plot(points[simplex, 0], points[simplex, 1], 'lightgray',zorder=-1)
 
 
 
