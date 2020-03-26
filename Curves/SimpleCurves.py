2ad3292c053e76b38a69dfe50091d591b3c4da2c
SymmetricChaos <ajfraebel@gmail.com>
2020-03-26 14:41:59 -0400

    Update SimpleCurves.py

--- Curves/SimpleCurves.py
+++ Curves/SimpleCurves.py
@@ -1,5 +1,13 @@
 import numpy as np
 
+def circle(r=1,n=1001):
+    th = np.linspace(0,2*np.pi,n)
+    x = r*np.sin(th)
+    y = r*np.cos(th)
+    
+    return x,y
+
+
 def ellipse(a,b,n=1001):
     th = np.linspace(0,2*np.pi,n)
     x = a*np.sin(th)
@@ -8,10 +16,23 @@ def ellipse(a,b,n=1001):
     return x,y
 
 
-def circle(r,n=1001):
+# Calculate ellipse from eccentricity and a scale factor
+def ellipse_ecc(e,s=1,n=1001):
+    
+    if e < 0:
+        raise Exception("Not a valid conic section")
+    if e == 0:
+        return circle(s,n)
+    if e == 1:
+        raise Exception("This eccentricity produces a parabola")
+    if e > 1:
+        raise Exception("This eccentricity produces a hyperbola")
+        
+    a = s
+    b = s*np.sqrt(1-(e*e))
     th = np.linspace(0,2*np.pi,n)
-    x = r*np.sin(th)
-    y = r*np.cos(th)
+    x = a*np.sin(th)
+    y = b*np.cos(th)
     
     return x,y
 
@@ -24,30 +45,20 @@ def parabola(a=1,xlim=[-1,1],n=1001):
 
 
 # The positive part of the conjugate parabola
-# Needs work for a better interface
+# Shows relationship with parabola and easier to work with when drawing
 def hyperbola(a,b,xlim=[-1,1],n=1001):
     x = np.linspace(xlim[0],xlim[1],n)
     y = a*np.sqrt(b*b+x*x)/b
 
     return x,y
 
-
-def conic(e,s=1,n=1001):
-    
-    # Circle
-    if e == 0:
-        return circle(s,n=n)
-    # Ellipse
-    elif e < 1:
-        a = s
-        b = s*np.sqrt(1-e*e)
-        return ellipse(a,b,n=n)
-    # Parabola
-    elif e ==1:
-        return parabola(s,n=n)
-    # Hyperbola
-    else:
-        return hyperbola()
+#def hyperbola(e,s,xlim=[-1,1],n=1001):
+#    a = s
+#    b = 
+#    x = np.linspace(xlim[0],xlim[1],n)
+#    y = a*np.sqrt(b*b+x*x)/b
+#
+#    return x,y
 
 
 def catenary(lo=-1,hi=1,n=1001):
@@ -56,10 +67,12 @@ def catenary(lo=-1,hi=1,n=1001):
     return x,y
 
 
-
-
 def polar_to_cart(r,th):
     return r*np.cos(th), r*np.sin(th)
+
+
+
+
 
 if __name__ == '__main__':
     
@@ -70,4 +83,10 @@ if __name__ == '__main__':
     plt.axes().set_aspect("equal","datalim")
     plt.plot(x1,y1-1)
     plt.plot(x2,y2)
+    plt.show()
+    
+    x,y = ellipse_ecc(.7,1.5)
+    plt.axes().set_aspect("equal","datalim")
+    plt.plot(x,y)
+    
     
