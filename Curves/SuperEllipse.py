d11c14c89d1685fe882d629506ba51599c9fedeb
SymmetricChaos <ajfraebel@gmail.com>
2020-03-31 11:01:42 -0400

    better handling of optional arg

--- Curves/SuperEllipse.py
+++ Curves/SuperEllipse.py
@@ -4,33 +4,10 @@ import numpy as np
 # https://web.archive.org/web/20171208231427/http://ftp.lyx.de/Lectures/SuperformulaU.pdf
 
 
-def superellipse(a=1,b=1,m=1,n=1001,draw=True):
+def superellipse(a=1,b=1,m1=2,m2=None,n=1001,draw=True):
     
-    th = np.linspace(0,2*np.pi,n)
-    
-    sin_vals = np.sin(th)
-    cos_vals = np.cos(th)
-    
-    
-    x = a*np.abs(sin_vals)**(2/m)*np.sign(sin_vals)
-    y = b*np.abs(cos_vals)**(2/m)*np.sign(cos_vals)
-    
-    if draw == True:
-        
-        fig = plt.figure()
-        fig.set_size_inches(12,12)
-        ax = plt.axes()
-        ax.set_aspect("equal","datalim")
-        ax.axis('off')
-        ax.set_xticks([])
-        ax.set_yticks([])
-        
-        plt.plot(x,y)
-    
-    return x,y
-
-
-def superellipse_mod(a=1,b=1,m1=1,m2=1,n=1001,draw=True):
+    if m2 == None:
+        m2 = m1
     
     th = np.linspace(0,2*np.pi,n)
     
@@ -94,4 +71,4 @@ if __name__ == '__main__':
 #    x,y = superformula(a=1,b=1,n2=1,n3=1,n1=2,m1=8,m2=3,turns=2,n=4001)
 #    x,y = superformula(m1=2,m2=2,n1=.5,n2=.5,n3=.5)
     x,y = superellipse(7,5,3)
-    x,y = superellipse_mod(7,5,1,2)
+    x,y = superellipse()
