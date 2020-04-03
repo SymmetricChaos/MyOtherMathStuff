e35f5ac3fb0134b8937f2a0d245a82d942ebb640
SymmetricChaos <ajfraebel@gmail.com>
2020-04-03 08:51:01 -0400

    Update Conversions.py

--- Curves/Conversions.py
+++ Curves/Conversions.py
@@ -1,11 +1,11 @@
 def xy_to_complex(x,y):
-    return [a+bj for a,b in zip(x,y)]
+    return [a+b*1j for a,b in zip(x,y)]
 
 def xy_to_point(x,y):
-    return [a,b for a,b in zip(x,y)]
+    return [(a,b) for a,b in zip(x,y)]
 
 def point_to_complex(P):
-    return [a+bj for a,b in P]
+    return [a+b*1j for a,b in P]
 
 def point_to_xy(P):
     return [p[0] for p in P], [p[1] for p in P]
@@ -14,4 +14,22 @@ def complex_to_xy(C):
     return [z.real for z in C], [z.imag for z in C]
 
 def complex_to_point(C):
-    return [z.real,z.imag for z in C]
+    return [(z.real,z.imag) for z in C]
+
+
+if __name__ == '__main__':
+    x = [1,2,3,4,5]
+    y = [5,4,3,2,1]
+    P = [(1,5),(2,4),(3,3),(4,2),(5,1)]
+    C = [1+5j,2+4j,+3+3j,4+2j,5+1j]
+    
+    print(x,y)
+    print(P)
+    print(C)
+    
+    print(xy_to_complex(x,y) == C)
+    print(xy_to_point(x,y) == P)
+    print(point_to_complex(P) == C)
+    print(point_to_xy(P) == (x,y))
+    print(complex_to_xy(C) == (x,y))
+    print(complex_to_point(C) == P)
