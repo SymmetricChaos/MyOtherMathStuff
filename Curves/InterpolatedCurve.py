69fc3f912cacdced77e82266197f0b6b8fbbeda7
SymmetricChaos <ajfraebel@gmail.com>
2020-04-07 12:13:40 -0400

    returns a function now

--- Curves/InterpolatedCurve.py
+++ Curves/InterpolatedCurve.py
@@ -1,3 +1,4 @@
+# Treat lists as polynomial coefficients and multiplies them
 def poly_mult(P, Q):
     
     out = [0]*(len(P)+len(Q))
@@ -17,9 +18,10 @@ def poly_mult(P, Q):
 
 
 # Interpolation using lagrange polynomials
-# Need to adapt this from SimpleCAS
 def lagrange_interpolation(X,Y):
+    
     final = []
+    
     for x,y in zip(X,Y):
         out = [y]
         for m in X:
@@ -28,11 +30,36 @@ def lagrange_interpolation(X,Y):
                 P = [-m,1]
                 out = poly_mult(out,poly_mult(P,d))
         final += out
-    return final
+    
+    # Probably a more efficient way to do this
+    def polynomial_func(x):
+        
+        c = 0
+        for pwr,co in enumerate(final):
+            c = c + co*(x**pwr)
+        return c
+        
+    return polynomial_func
 
 
 
 
 
 if __name__ == '__main__':
-    print(lagrange_interpolation([0,1,2],[1,1,0]))
+    
+    import numpy as np
+    import matplotlib.pyplot as plt
+    
+    X = [1,3,4]
+    Y = [1,2,1.5]
+    P = lagrange_interpolation(X,Y)
+    
+    x = np.linspace(X[0]-1,X[-1]+1,1001)
+    y = [P(i) for i in x]
+    
+    fig = plt.figure()
+    fig.set_size_inches(6,6)
+    ax = plt.axes(xlim=[-2,5],ylim=[-3,3])
+        
+    plt.scatter(X,Y)
+    plt.plot(x,y)
