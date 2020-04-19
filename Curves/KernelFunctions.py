3dafbda6c07cf94d004c21c2abd9b85bf174a735
SymmetricChaos <ajfraebel@gmail.com>
2020-04-19 13:45:30 -0400

    Update KernelFunctions.py

--- Curves/KernelFunctions.py
+++ Curves/KernelFunctions.py
@@ -32,6 +32,18 @@ def parametric_kernel(a,b):
     return func
 
 
+def parametric_weights(a,b):
+        
+    def func(u):
+        if abs(u) > 1:
+            return 0
+        return (1-abs(u)**a)**b
+
+    return func
+
+
+
+
 
 if __name__ == '__main__':
     x = np.linspace(-1.2,1.2,101)
@@ -42,6 +54,5 @@ if __name__ == '__main__':
     
     for a,b in zip([1,1,1,2,2,2,3,3,3],[1,2,3,1,2,3,1,2,3]):
         f = parametric_kernel(a,b)
-        y = [f(i) for i in x]
         
-        plt.plot(x,y)
+        plt.plot(x,[f(i) for i in x])
