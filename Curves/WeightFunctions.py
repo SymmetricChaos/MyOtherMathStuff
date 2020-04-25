df4f5debb09406c293f28a0309a2362d06ed4d68
SymmetricChaos <ajfraebel@gmail.com>
2020-04-24 23:04:04 -0400

    Update WeightFunctions.py

--- Curves/WeightFunctions.py
+++ Curves/WeightFunctions.py
@@ -2,45 +2,55 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Common weight functions.
-# These take a list of values between -1 and 1 and assign a value to each to
-# represent its "importance" in some sense. Then normalize the result to sum to
-# one.
+# These take a list of values assign a value to each to represent its
+# "importance" in some sense. Not normalized
 
 # Convenience function to take a range of values, center the mean on zero, and
 # divide by the largest absolute value.
-def center_and_squish(L):
+def center_and_squish(L,c=None,m=None):
     
-    c = np.mean(L)
+    if c == None:
+        c = np.mean(L)
     out = [l-c for l in L]
-    m = max([abs(o) for o in out])
+    
+    if m == None:
+        m = max([abs(o) for o in out])
     
     return [o/m for o in out]
 
-def triangular_weights(U):
+
+# Goes to zero very quickly but covers the whole real line
+def exponential_weights(U,e=2):
     
-    out = [1-abs(u) for u in U]
+    out = [e**-abs(u) for u in U]
     out = [max(val,0) for val in out]
-    S = sum(out)
     
-    return [val/S for val in out]
+    return out
 
 
 # Rebuild to make functions based on triangular_weight above
 # Creates a functions from a paramatric family of weight functions based on the
-# triangular. They give differing amounts of peakedness.
+# triangular. They give differing amounts of peakedness. Limited to the interval
+# [-1,1]
 def parametric_weights(a=1,b=1):
     
     def func(U):
         out = [(1-abs(u)**a)**b if abs(u) <= 1 else 0 for u in U]
-        S = sum(out)
+
         
-        return [val/S for val in out]
+        return out
 
     return func
 
-
-#def exponential_weights(u):
+# Common special case of parametric weights
+def triangular_weights(U):
     
+    out = [1-abs(u) for u in U]
+    out = [max(val,0) for val in out]
+    
+    return out
+
+
 
 
 
@@ -52,13 +62,13 @@ if __name__ == '__main__':
     
     make_blank_canvas()
     
-    x = np.linspace(-1.2,1.2,101)    
+    x = np.linspace(-1.2,1.2,51)    
     ctr = 1
     for a,b in zip([1,1,1,2,2,2,3,3,3],[1,2,3,1,2,3,1,2,3]):
         make_blank_subplot(3,3,ctr)
         f = parametric_weights(a,b)
         
-        plt.plot(x,[y*30 for y in f(x)],color="gray",alpha=.5)
+        plt.plot(x,[y for y in f(x)],color="gray",alpha=.5)
         if a == 1:
             a_sym = ""
         else:
@@ -69,3 +79,9 @@ if __name__ == '__main__':
             b_sym = "^"+str(b)
         plt.title(f"$(1-|u|{a_sym}){b_sym}$")
         ctr += 1
+
+    y = exponential_weights(x,4)
+    
+    make_blank_canvas()
+    plt.plot(x,y)
+    
