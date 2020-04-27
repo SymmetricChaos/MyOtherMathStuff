e7552264ae204297e26515c4eda94caadca3f827
SymmetricChaos <ajfraebel@gmail.com>
2020-04-27 13:49:31 -0400

    Update MovingAveragesDash.py

--- Curves/MovingAveragesDash.py
+++ Curves/MovingAveragesDash.py
@@ -7,14 +7,15 @@ from MovingAverages import simple_moving_average, weighted_moving_average
 from WeightFunctions import triangular_weights, exponential_weights
 import numpy as np
 
+# Precreate some simple data
+x1 = list(np.linspace(0,7,150))
+y1 = list(np.cos(x1))
+y2 = list(np.cos(x1)+np.random.normal(0,.4,150))
 
-x1 = list(np.linspace(0,7,300))
-y1 = list(np.cos(x1)+np.random.normal(0,.4,300))
-
-
+# Create the webapp
 app = dash.Dash()
 
-
+# Simple layout with only a dropdown to select the type of moving average
 app.layout = html.Div([
     dcc.Dropdown(
         id='dropdown',
@@ -25,31 +26,35 @@ app.layout = html.Div([
         ],
         value='simple'
     ),
-    html.Div(id='dd-output-container')
+    
+
+    
+    html.Div(id='dd-output-container'),
+    
 ])
 
 
-
-
+# A function that can update the graph based on making callback to the dropdown
 @app.callback(
     Output(component_id='dd-output-container', component_property='children'),
     [Input(component_id='dropdown', component_property='value')]
 )
 def update_value(input_data):
+    
     U = np.linspace(-1,1,14)
     t_weights = triangular_weights(U)
     e_weights = exponential_weights(U)
     
-    Y = {"simple": simple_moving_average(y1,6),
-         "triangular": weighted_moving_average(y1,6,t_weights),
-         "exponential": weighted_moving_average(y1,6,e_weights)
+    Y = {"simple": simple_moving_average(y2,6),
+         "triangular": weighted_moving_average(y2,6,t_weights),
+         "exponential": weighted_moving_average(y2,6,e_weights)
         }
     
     return dcc.Graph(
         id='example-graph',
         figure={
             'data': [
-                {'x': x1, 'y': y1, 'type': 'line', 'name': "data"},
+                {'x': x1, 'y': y2, 'type': 'line', 'name': "data"},
                 {'x': x1, 'y': Y[str(input_data)], 'type': 'line', 'name': "model"},
             ],
             'layout': {
@@ -57,6 +62,8 @@ def update_value(input_data):
             }
         }
     )
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
