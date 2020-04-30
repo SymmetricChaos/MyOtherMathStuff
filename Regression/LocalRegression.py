3c093bacc1bdba325553365e5d8eeb845dea036e
SymmetricChaos <ajfraebel@gmail.com>
2020-04-30 08:54:20 -0400

    Filling out regression folder

---
+++ Regression/LocalRegression.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from Conversions import xy_to_points, points_to_xy
+from Drawing import make_blank_canvas, draw_curve_xy, draw_dots_xy
+from PolynomialRegression import polynomial_regression, poly
+
+# Basically works but a bit unstable
+def simple_local_regression(X,Y,width,n,degree=1):
+    
+    C = np.linspace(min(X),max(X),n)
+    P = xy_to_points(X,Y)
+    
+    out = []
+    
+    def inrange(x,c,w):
+        return x>=c-w and x<=c+w
+    
+    for cen in C:
+        xy = [p for p in P if inrange(p[0],cen,width)]
+        if xy == []:
+            raise Exception(f"Width is too narrow at x={cen}")
+        x,y = points_to_xy(xy)
+        coef = polynomial_regression(x,y,degree=degree)
+        
+        out.append(poly(cen,coef))
+
+    return C,out
+
+
+
+if __name__ == '__main__':
+    x_data = x_base = np.linspace(-1,6,300)
+    y_base = np.cos(x_data)
+    y_data = y_base+np.random.normal(0,.3,300)
+    
+    
+    x1,y1 = simple_local_regression(x_data,y_data,width=.5,n=100,degree=1)
+    
+    make_blank_canvas(xlim=(-2,7),ylim=(-2,2),size=(12.5,6))
+    draw_dots_xy(x_data,y_data,s=5)
+    draw_curve_xy(x_base,y_base)
+    draw_curve_xy(x1,y1,color='red')
+    plt.title("Simple Local Linear Regression Curve",size=20)
--- Regression/PolynomialRegression.py
+++ Regression/PolynomialRegression.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from Conversions import xy_to_points, points_to_xy
+#from Conversions import xy_to_points, points_to_xy
 from Drawing import make_blank_canvas, draw_curve_xy, draw_dots_xy
 
 
@@ -9,7 +9,7 @@ def residual(x,y,f):
     return y - f(x)
 
 
-# Polynomial function
+# Quick function to evaluate a polynomial function
 def poly(x,coefs):
     out = 0
     for p,c in enumerate(coefs):
@@ -54,27 +54,7 @@ def bagged_regression(X,Y,degree=1,runs=20):
         yield polynomial_regression(x_sub,y_sub,degree)
         
     
-# Basically works but a bit unstable
-def simple_local_regression(X,Y,width,n,degree=1):
-    
-    C = np.linspace(min(X),max(X),n)
-    P = xy_to_points(X,Y)
-    
-    out = []
-    
-    def inrange(x,c,w):
-        return x>=c-w and x<=c+w
-    
-    for cen in C:
-        xy = [p for p in P if inrange(p[0],cen,width)]
-        if xy == []:
-            raise Exception(f"Width is too narrow at x={cen}")
-        x,y = points_to_xy(xy)
-        coef = polynomial_regression(x,y,degree=degree)
-        
-        out.append(poly(cen,coef))
 
-    return C,out
 
 
 
@@ -105,9 +85,4 @@ if __name__ == '__main__':
     plt.title("4th Degree Polynomial Regression Curves on Random Subsets",size=20)
 
 
-    make_blank_canvas(xlim=(-2,7),ylim=(-2,2),size=(12.5,6))
-    draw_dots_xy(x1,y1,s=5)
-    draw_curve_xy(x3,y3)
-    draw_curve_xy(x1,y0,color='red')
-    plt.title("Simple Local Linear Regression Curve",size=20)
     
