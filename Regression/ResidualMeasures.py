a4700b56ae580269182dc752c3354f410f08849e
SymmetricChaos <ajfraebel@gmail.com>
2020-05-03 10:49:28 -0400

    Update ResidualMeasures.py

--- Regression/ResidualMeasures.py
+++ Regression/ResidualMeasures.py
@@ -1,19 +1,32 @@
 
 # Differences between prediction funcion f(x) and data y
 # Optionally raised to a power, usually squared
-def residuals(X,Y,f,p=1):
-    devs = []
-    for x,y in X,Y:
-        devs.append( abs(y-f(x))**p )
-    
-    return devs
+def residuals(X,Y,f,p=1):    
+    return [abs(y-f(x))**p for x,y in zip(X,Y)]  
 
 
 # Differences between some central point c and actual values
 # Optionally raised to a power, usually squared
 def errors(D,c,p=1):
-    devs = []
-    for d in D:
-        devs.append( abs(d-c)**p )
+    return [abs(d-c)**p for d in D]
+
+
+
+
+
+if __name__ == '__main__':
     
-    return devs
+    import numpy as np
+    
+    x = np.linspace(0,10,100)
+    y = x+np.random.standard_normal(100)
+    
+    def model(x):
+        return x
+    
+    median_abs_dev = np.median(residuals(x,y,model))
+    mean_sq_dev = np.mean(residuals(x,y,model,p=2))
+    
+    print("Data with Standard Normal Error Term")
+    print(f"Median Absolute Deviations: {median_abs_dev:.3}")
+    print(f"Mean Squared Deviations:    {mean_sq_dev:.3}")
