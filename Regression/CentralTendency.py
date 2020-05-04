0f7a3d4e2c77dbce3ba2a571688735b4aad5c35a
SymmetricChaos <ajfraebel@gmail.com>
2020-05-04 11:03:01 -0400

    Update CentralTendency.py

--- Regression/CentralTendency.py
+++ Regression/CentralTendency.py
@@ -2,22 +2,40 @@ import numpy as np
 
 def median(L):
     L = sorted(L)
+    parity = len(L)%2
     half_range = len(L)//2
+    
+    if parity == 1:
+        return L[half_range]
+    else:
+        return (L[half_range]+L[half_range+1])/2
     
 
 #def weighted_median(L,w):
-    
+
+
 def mean(L):
     return sum(L)/len(L)
 
+
 # Arithmetic mean with quartiles beyond t and 1-t removed
 #def truncated_mean(L,t=.05):
-    
-    
+
+
     
 #def weighted_mean(L,w):
-#    
+
+
 #def geometric_mean(L):
-#    
+
+
 #def harmonic_mean(L):
-#    
+
+
+if __name__ == '__main__':
+    
+    A = [1,2,3,4,5]
+    B = [1,2,3,4]
+    
+    print(median(A))
+    print(median(B))
