d58580099b67d02dbcca55a92671c6428135e5b5
SymmetricChaos <ajfraebel@gmail.com>
2020-05-04 11:00:02 -0400

    Dispersion and Central Tendency

---
+++ Regression/CentralTendency.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def median(L):
+    L = sorted(L)
+    half_range = len(L)//2
+    
+
+#def weighted_median(L,w):
+    
+def mean(L):
+    return sum(L)/len(L)
+
+# Arithmetic mean with quartiles beyond t and 1-t removed
+#def truncated_mean(L,t=.05):
+    
+    
+    
+#def weighted_mean(L,w):
+#    
+#def geometric_mean(L):
+#    
+#def harmonic_mean(L):
+#    
---
+++ Regression/Dispersion.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+# Interquartile range
+def IQR(L):
+    L = sorted(L)
+
+# Standard deviation
+#def SD(L):
