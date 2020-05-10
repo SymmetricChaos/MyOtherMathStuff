2e85c4ba721dcd41c1d9b5084b15dfd6a0c19891
SymmetricChaos <ajfraebel@gmail.com>
2020-05-10 14:18:59 -0400

    Ordinals

---
+++ Fractals/OrdinalRecursive.py
@@ -0,0 +1,23 @@
+def ordinals():
+    n = "{}"
+    while True:
+        yield n
+        n = "{" + n[1:-1] + n + "}"
+
+
+
+
+
+if __name__ == '__main__':
+    
+    
+    print("We can define an ordinal as the set containing all the ordinals before it.")
+    print("This can be done without reference to the idea of 'the ordinals before it' simply by defining a successor function that builds the next ordinal as the union of itself with the set that contains it.")
+    print("This makes it possible to define an order on them so that a < b iff a is an element of b.")
+    
+    ords = []
+    for n,o in enumerate(ordinals()):
+        print(n,o)
+        ords.append(o)
+        if n > 4:
+            break
--- Utils/Drawing.py
+++ Utils/Drawing.py
@@ -283,6 +283,7 @@ def text(x,y,t,ax=None,**kwargs):
 
 
 
+
 if __name__ == '__main__':
     
     import numpy as np
