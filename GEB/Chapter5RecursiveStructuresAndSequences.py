23b228c5bd83ea7219ac0ef9282c0434ca54fa97
SymmetricChaos <ajfraebel@gmail.com>
2020-05-11 11:49:17 -0400

    Update Chapter5RecursiveStructuresAndSequences.py

--- GEB/Chapter5RecursiveStructuresAndSequences.py
+++ GEB/Chapter5RecursiveStructuresAndSequences.py
@@ -1,3 +1,4 @@
+import numpy as np
 def G(n):
     if n == 0:
         return 0
@@ -32,26 +33,64 @@ def Q(n):
         return Q(n-Q(n-1)) + Q(n-Q(n-2))
 
 
-#def OrnateNoun():
-    # Article
-    # Adjectives
-    # Noun
+# RTN
+adj_list = ["acidic","basic","small","large","hot","cold","bright","dark","red","green","blue","orange","tall","short","clean","dirty"]
+noun_list = ["animal","plant","rock","computer","book","desk","fan","bottle","person","oven","country","thing","child","adult","road"]
+prep_list = ["about","above","across","after","against","among","around","at","before","behind","below","beside","between","by","down","during","except","for","from","in","inside","into","near","of","off","on","out","over","through","to","toward","under","up","with"]
+verb_list = ["chose","wore","bought","flew","dropped","grabbed","ate","climbed","threw","jumped"]
+rel_pronoun_list = ["who","which","that","whom"]
+
+
+def Article():
+    return np.random.choice(["a","an","the"],1)[0]
+
+def Adjective():
+    return np.random.choice(adj_list,1)[0]
+
+def Noun():
+    return np.random.choice(noun_list,1)[0]
+
+def Preposition():
+    return np.random.choice(prep_list,1)[0]
+
+def Verb():
+    return np.random.choice(verb_list,1)[0]
+
+def RelativePronoun():
+    return np.random.choice(rel_pronoun_list,1)[0]
+
+
+def OrnateNoun():
+    S = ""
+    S += Article() + " "
+    while np.random.uniform() > .5:
+        S += Adjective() + " "
+    S += Noun()
+    return S
     
     
-#def FancyNoun():
-    # OrnateNoun
-    # Preposition
-    # FancyNoun
+def FancyNoun():
+    S = ""
+    path = np.random.randint(0,2)
     
-    # OrnateNoun
-    # RelativePronoun
-    # Verb
-    # FancyNoun
+    if path == 0:
+        S += OrnateNoun() + " "
+        S += Preposition() + " "
+        S += FancyNoun()
     
-    # OrnateNoun
-    # RelativePronoun
-    # FancyNoun
-    # Verb
+    elif path == 1:
+        S += OrnateNoun() + " "
+        S += RelativePronoun() + " "
+        S += Verb() + " "
+        S += OrnateNoun()
+    
+    elif path == 2:
+        S += OrnateNoun() + " "
+        S += RelativePronoun() + " "
+        S += OrnateNoun() + " "
+        S += Verb()
+        
+    return S
 
     
 
@@ -60,17 +99,21 @@ def Q(n):
 
 if __name__ == '__main__':
     
+    # Ornate Noun
+    print(f"Ornate noun: {OrnateNoun()}")
+    print(f"Fancy noun: {FancyNoun()}")
+    
     #Recreate the graphs from the book
     
-    print("Diagram G")
+    print("\nDiagram G")
     for i in range(25):
         print(G(i),end=" ")
     
-    print("\nDiagram H")
+    print("\n\nDiagram H")
     for i in range(25):
         print(H(i),end=" ")
         
-    print("\nDiagram Q")
+    print("\n\nDiagram Q")
     for i in range(1,25):
         print(Q(i),end=" ")
         
