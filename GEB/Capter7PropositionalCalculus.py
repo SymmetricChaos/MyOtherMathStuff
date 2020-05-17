3ef513a74cab7e54c351fa350f708f37ad238d14
SymmetricChaos <ajfraebel@gmail.com>
2020-05-17 07:19:43 -0400

    Update Capter7PropositionalCalculus.py

--- GEB/Capter7PropositionalCalculus.py
+++ GEB/Capter7PropositionalCalculus.py
@@ -5,7 +5,7 @@ from Utils.StringManip import bracket_matching
 
 # Determine if the input is an atomic well-formed string
 def is_atom(x):
-    if re.match("^[PQR]\'*$",x):
+    if re.match("^~*[PQR]\'*$",x):
         return True
     return False
 
@@ -22,6 +22,31 @@ def IF(x,y):
 def NOT(x):
     return f"~{x}"
 
+def is_well_formed(x):
+    strings = bracket_matching(x,"<>")
+    sub_strings = strings[:-1]
+
+    #Little debugger
+    #print(f"\ntesting {x}")
+    
+    # If there are no lower substrings
+    if len(sub_strings) == 0:
+        # If its a simple formula then it is well-formed
+        if re.match("<~*[PQR]'*[∧∨⊃]~*[PQR]'*>",x):
+            return True
+        # If its an atom then it is well-formed
+        else:
+            return is_atom(x)
+    
+    # Otherwise check every substring for being well formed and return false
+    # If any of them are false
+    else:
+        for s in sub_strings:
+            if not is_well_formed(s[0]):
+                return False
+    
+    # Otherwise it must be well-formed
+    return True
 
 
 
@@ -38,9 +63,12 @@ if __name__ == '__main__':
     print(AND(P,NOT(Qp)))
     
     print("\nTo decompose well-formed strings we need to be able to find bracketed sections. Consider the well-formed string below.")
-    s = "<~<P∧~Q'>∨<~~<P∧R>⊃Q'>>"
+    s = "<~<P∧~Q'>∨<~<~P∧R>⊃Q'>>"
     print(s)
     print("The bracketed parts of the string are:")
     braks = bracket_matching(s,"<>")
     for i in braks:
         print(i[0],i[1:3])
+    
+    print("\n\n")
+    print(is_well_formed(s))
