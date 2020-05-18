7fcccf7dc3ebbbbe823213acb1f3dd0ee1b3b4f1
SymmetricChaos <ajfraebel@gmail.com>
2020-05-18 08:16:15 -0400

    Update StringManip.py

--- Utils/StringManip.py
+++ Utils/StringManip.py
@@ -11,13 +11,21 @@ def bracket_matching(S,parens="()",overlap=True,inner=False):
         if S[pos] == parens[0]:
             left.append(pos)
         if S[pos] == parens[1]:
-            try:
-                spans.append( (left.pop(),pos) )
-            except:
-                raise Exception(f"Too many right brackets")
+            if overlap:
+                try:
+                    spans.append( (left.pop(),pos) )
+                except:
+                    print("Warning: Too many right brackets")
+            else:
+                try:
+                    L = left.pop()
+                    if len(left) == 0:
+                        spans.append( (L,pos) )
+                except:
+                    print("Warning: Too many right brackets")
     
     if len(left) != 0:
-        raise Exception(f"Too many left brackets")
+        print("Warning: Too many left brackets")
 
     # output is list with the subsections and the spans
     output = []
@@ -29,19 +37,19 @@ def bracket_matching(S,parens="()",overlap=True,inner=False):
             output.append((S[lo:hi+1],lo,hi))
 
     # If overlap is false filter out any string contained in another string
-    if not overlap:
-        filtered_output = []
-        for a in output:
-            outside_all = True
-            for b in output:
-                if a[1] == b[1]:
-                    continue
-                elif a[1] > b[1] and a[2] < b[2]:
-                    outside_all = False
-                    break
-            if outside_all:
-                filtered_output.append(a)
-        output = filtered_output
+#    if not overlap:
+#        filtered_output = []
+#        for a in output:
+#            outside_all = True
+#            for b in output:
+#                if a[1] == b[1]:
+#                    continue
+#                elif a[1] > b[1] and a[2] < b[2]:
+#                    outside_all = False
+#                    break
+#            if outside_all:
+#                filtered_output.append(a)
+#        output = filtered_output
     
     return output
 
@@ -56,8 +64,16 @@ if __name__ == '__main__':
     braks2 = bracket_matching(s,inner=True)
     braks3 = bracket_matching(s,overlap=False)
     
+    explanations = ["find every pair of matched parentheses",
+                    "same as before but returning only contents of parentheses",
+                    "find the parentheses that are outermost"]
+    
     print(s)
-    for b in [braks1,braks2,braks3]:
-        print()
+    for e,b in zip(explanations,[braks1,braks2,braks3]):
+        print(f"\n{e}")
         for i in b:
             print(i[0])
+            
+    
+            
+    
