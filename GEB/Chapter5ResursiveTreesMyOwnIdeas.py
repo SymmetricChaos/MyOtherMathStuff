05d27e7a4d851edcd3f43751c103598f5c6af7e7
SymmetricChaos <ajfraebel@gmail.com>
2020-05-20 12:22:52 -0400

    Update Chapter5ResursiveTreesMyOwnIdeas.py

--- GEB/Chapter5ResursiveTreesMyOwnIdeas.py
+++ GEB/Chapter5ResursiveTreesMyOwnIdeas.py
@@ -3,71 +3,107 @@ import numpy as np
 
 # Complex recursive tree
 
-def F_graph1(root,scale=0,ax=None):
+def Graph1(root,scale=0,ax=None):
     s = 1/(2**scale)
     node1 = root
     node2 = [ root[0]-5*s , root[1]+1 ]
     leaf1 = [ root[0]+5*s , root[1]+1 ]
     leaf2 = [ root[0]-5*s , root[1]+2 ]
     
-    draw.draw_circle_p(node1,R=.1,ax=ax)
-    draw.draw_circle_p(node2,R=.1,ax=ax)
-    draw.connect_p(node1,node2)
-    draw.connect_p(node2,leaf2)
-    draw.connect_p(node1,leaf1)
+    draw.draw_circle_p(node1,R=.06,ax=ax)
+    draw.draw_circle_p(node2,R=.06,ax=ax)
+    draw.connect_p(node1,node2,linewidth=1)
+    draw.connect_p(node2,leaf2,linewidth=1)
+    draw.connect_p(node1,leaf1,linewidth=1)
 
     return leaf1,leaf2
 
-def F_graph2(root,scale=0,ax=None):
+def Graph2(root,scale=0,ax=None):
     s = 1/(2**scale)
     node1 = root
     node2 = [ root[0]+5*s , root[1]+1 ]
     leaf1 = [ root[0]-5*s , root[1]+1 ]
     leaf2 = [ root[0]+5*s , root[1]+2 ]
     
-    draw.draw_circle_p(node1,R=.1,ax=ax)
-    draw.draw_circle_p(node2,R=.1,ax=ax)
-    draw.connect_p(node1,node2)
-    draw.connect_p(node1,leaf1)
-    draw.connect_p(node2,leaf2)
+    draw.draw_circle_p(node1,R=.06,ax=ax)
+    draw.draw_circle_p(node2,R=.06,ax=ax)
+    draw.connect_p(node1,node2,linewidth=1)
+    draw.connect_p(node1,leaf1,linewidth=1)
+    draw.connect_p(node2,leaf2,linewidth=1)
 
     return leaf1,leaf2
 
-def F_graph_recur1(root,levels=1,scale=0,ax=None):
-
-    if scale >= levels:
-        return 0
-    else:
-        leaf1,leaf2 = F_graph1(root,scale,ax)
-        if scale % 2 == 0:
-            F_graph_recur2(leaf1,levels,scale+1,ax)
-            F_graph_recur1(leaf2,levels,scale+1,ax)
-        else:
-            F_graph_recur1(leaf1,levels,scale+1,ax)
-            F_graph_recur2(leaf2,levels,scale+1,ax)
-        
-def F_graph_recur2(root,levels=1,scale=0,ax=None):
-
-    if scale >= levels:
-        return 0
-    else:
-        leaf1,leaf2 = F_graph2(root,scale,ax)
-        F_graph_recur1(leaf1,levels,scale+1,ax)
-        F_graph_recur2(leaf2,levels,scale+1,ax)
-        
-def F_graph_example():
-    draw.make_blank_canvas([-10,10],[-5,15],[14,14])
-    F_graph_recur1([0,0],5)
+def Graph3(root,scale=0,ax=None):
+    s = 1/(2**scale)
+    node1 = root
+    node2 = [ root[0]+5*s , root[1]+1 ]
+    node3 = [ root[0]-5*s , root[1]+1 ]
+    leaf1 = [ root[0]+5*s , root[1]+2 ]
+    leaf2 = [ root[0]-5*s , root[1]+2 ]
     
-    # Show the components
-    draw.draw_rect_xy(-3,11.5,-1,14.5,ec='black',fc='white',zorder=-1)
-    F_graph1([-2,12],3)
-    draw.draw_rect_xy(1,11.5,3,14.5,ec='black',fc='white',zorder=-1)
-    F_graph2([2,12],3)
+    draw.draw_circle_p(node1,R=.06,ax=ax)
+    draw.draw_circle_p(node2,R=.06,ax=ax)
+    draw.draw_circle_p(node3,R=.06,ax=ax)
+    
+    draw.connect_p(node1,node2,linewidth=1)
+    draw.connect_p(node1,node3,linewidth=1)
+    draw.connect_p(node2,leaf1,linewidth=1)
+    draw.connect_p(node3,leaf2,linewidth=1)
 
+    return leaf1,leaf2
+
+
+def Graph1_recur(root,levels=1,scale=0,ax=None):
+
+    if scale >= levels:
+        return 0
+    else:
+        leaf1,leaf2 = Graph1(root,scale,ax)
+        l,r = np.random.choice([1,2,3],2)
+        GDict[l](leaf1,levels,scale+1,ax)
+        GDict[l](leaf2,levels,scale+1,ax)
+        
+        
+def Graph2_recur(root,levels=1,scale=0,ax=None):
+
+    if scale >= levels:
+        return 0
+    else:
+        leaf1,leaf2 = Graph2(root,scale,ax)
+        l,r = np.random.choice([1,2,3],2)
+        GDict[l](leaf1,levels,scale+1,ax)
+        GDict[l](leaf2,levels,scale+1,ax)
+        
+def Graph3_recur(root,levels=1,scale=0,ax=None):
+
+    if scale >= levels:
+        return 0
+    else:
+        leaf1,leaf2 = Graph3(root,scale,ax)
+        l,r = np.random.choice([1,2,3],2)
+        GDict[l](leaf1,levels,scale+1,ax)
+        GDict[l](leaf2,levels,scale+1,ax)     
+
+
+GDict = {1:Graph1_recur, 
+         2:Graph2_recur, 
+         3:Graph3_recur}
+        
+def RandomTreeExample():
+    draw.make_blank_canvas([-10,10],[-5,15],[14,14])
+    base, = np.random.choice([1,2,3],1)
+    GDict[base]([0,-2],6)
+    draw.title("Randomized Tree",size=22)
+    # Show the components
+    draw.draw_rect_xy(-5,11.5,-3,14.5,ec='black',fc='white',zorder=-1)
+    Graph1([-4,12],3)
+    draw.draw_rect_xy(3,11.5,5,14.5,ec='black',fc='white',zorder=-1)
+    Graph2([4,12],3)
+    draw.draw_rect_xy(-1,11.5,1,14.5,ec='black',fc='white',zorder=-1)
+    Graph3([0,12],3)
 
     
 if __name__ == '__main__':
     
-    F_graph_example()
+    RandomTreeExample()
     draw.show_now()
