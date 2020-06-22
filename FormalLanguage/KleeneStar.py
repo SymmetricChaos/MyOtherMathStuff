9b0ef433c63d91e4d34454ac628130466a7609d2
SymmetricChaos <ajfraebel@gmail.com>
2020-06-22 11:44:57 -0400

    Update KleeneStar.py

--- FormalLanguage/KleeneStar.py
+++ FormalLanguage/KleeneStar.py
@@ -1,5 +1,4 @@
 def kleene_finite(S,n):
-    S = set(S)
     if n == 0:
         yield ""
     else:
@@ -10,12 +9,26 @@ def kleene_finite(S,n):
                     unique.add(v+s)
                     yield v+s
 
-#def kleene_star(S):
-#    for s in S:
-
+def kleene_star(S):
+    unique = set([])
+    
+    ctr = 0
+    while True:
+        for s in kleene_finite(S,ctr):
+            if s not in unique:
+                unique.add(s)
+                yield s
+        ctr += 1
+    
 
 if __name__ == '__main__':
     
+    S = ['a','ab','ba','cc']
     for i in range(4):
-        print([k for k in kleene_finite(['ab','ba','aa'],i)])
+        K = [k for k in kleene_finite(S,i)]
+        print(f"V_{i} = {K}")
     
+    for n,s in enumerate(kleene_star(S)):
+        print(s)
+        if n > 30:
+            break
