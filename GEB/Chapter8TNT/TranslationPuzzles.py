2fbf699ac4dc28e28fea5d4cb413c43b8a12a3ab
SymmetricChaos <ajfraebel@gmail.com>
2020-06-23 14:56:26 -0400

    Update TranslationPuzzles.py

--- GEB/Chapter8TNT/TranslationPuzzles.py
+++ GEB/Chapter8TNT/TranslationPuzzles.py
@@ -1,20 +1,39 @@
 from GEB.Chapter8TNT.Rules import *
 from GEB.Chapter8TNT.Translate import translate
+from GEB.Chapter8TNT.Properties import is_well_formed
 
-divisibility = EXISTS("a*c=b","c")
-greater_than = EXISTS(AND("a+c=b","~a=b"),"c")
-prime = FOR_ALL(NOT("∃c:SSa*SSc=b"),"a")
+divisibility = "∃c:(a⋅c)=b"
+is_even = "∃a:(a⋅SS0)=b"
+greater_than = "∃c:<(a+c)=b∧~a=b>"
+prime = "∀a:~∃c:(SSa⋅SSc)=b"
+square = "∃a:(a⋅a)=b"
+square_root = "∃b:(a⋅a)=b"
+power_of_two = "<∃c:(a⋅c)=b⊃∃d:(d⋅SS0)=a>"
+
+
 
 explanations = ["b is divisible by a if",
+                "b is even if",
                 "b is greather than a if",
-                "b is prime if"]
+                "b is prime if",
+                "b is square if",
+                "a is the square root of b if",
+                "b is a power of two if"]
+
 theorems = [divisibility,
+            is_even,
             greater_than,
-            prime]
+            prime,
+            square,
+            square_root,
+            power_of_two]
 
 
 
 for ex,th in zip(explanations,theorems):
+    if not is_well_formed(th):
+        print(f"{th} is not well formed!")
+        break
     print(ex,translate(th))
     print(th)
     print()
