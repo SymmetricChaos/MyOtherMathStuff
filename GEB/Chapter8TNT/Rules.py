2b0c3e3de8833a85640e11a33d6f477ef0e99ddc
SymmetricChaos <ajfraebel@gmail.com>
2020-06-27 20:55:24 -0400

    restored transitivity example

--- GEB/Chapter8TNT/Rules.py
+++ GEB/Chapter8TNT/Rules.py
@@ -198,7 +198,7 @@ def induction(x,u,T):
 if __name__ == '__main__':
     
     print("\n\nRule of Specification")
-    print(f"{PeanoAxioms[1]} ⟹ {specify(PeanoAxioms[1],'a','(c+d)')}")
+    print(f"{PeanoAxioms[1]} ⟹ {specify(PeanoAxioms[1],'a','Sa')}")
     print(f"{PeanoAxioms[3]} ⟹ {specify(PeanoAxioms[3],'a','(S0⋅0)')}")
     print(f"{PeanoAxioms[4]} ⟹ {specify(PeanoAxioms[4],'b','(S0+b)')}")
     
@@ -219,17 +219,15 @@ if __name__ == '__main__':
     print(f"{PeanoAxioms[2]} ⟹ {existence(PeanoAxioms[2],'Sb','c')}")
     
     
-#    print("\n\n\nRule of Transitivity")
-#    trans_example1 = "(a+b)=(a+S0)"
-#    trans_example2 = "(a+S0)=S(a+0)"
-#    trans_example3 = AND(trans_example1,trans_example2)
-#    print(f"{trans_example1}")
-#    print(f"{trans_example2}")
-#    print(f"{trans_example3}")
-#    print(f"{transitivity(trans_example3)}")
+    print("\n\n\nRule of Transitivity")
+    trans_example1 = "(a+b)=(a+S0)"
+    trans_example2 = "(a+S0)=S(a+0)"
+    print(f"{trans_example1}")
+    print(f"{trans_example2}")
+    print(f"{transitivity(trans_example1,trans_example2)}")
 
 
     print("\n\n\nRule of Symmetry")
-    symm_example1 = "∀a:(a+0)=a"
+    symm_example1 = "(a+0)=a"
     print(f"{symm_example1}")
     print(f"{symmetry(symm_example1)}")
