260b6d6552f2fb2bae5c6117d6d008d59ee53aff
SymmetricChaos <ajfraebel@gmail.com>
2020-06-28 10:21:32 -0400

    MPLT respects the ax passing system

--- Utils/MatPlotTurtle.py
+++ Utils/MatPlotTurtle.py
@@ -3,10 +3,11 @@ import math
 
 class mplTurtle:
     
-    def __init__(self,pos=(0,0),angle=0,draw=True):
+    def __init__(self,pos=(0,0),angle=0,draw=True,ax=None):
         self.pos = pos
-        self.angle = angle
+        self.angle = angle%360
         self.draw = draw
+        self.ax = ax
         
     def left(self,n):
         self.angle = (self.angle+n)%360
@@ -19,32 +20,17 @@ class mplTurtle:
     
     def pen_down(self):
         self.draw = True
-    
-    def jump_to(self,pos):
-        self.pos = pos
         
     def move_to(self,pos):
         if self.draw:
-            Drawing.connect_p(self.pos,pos)
+            Drawing.connect_p(self.pos,pos,self.ax)
         else:
             self.pos = pos
-    
-    def set_angle(self,angle):
-        self.angle = angle
         
     def forward(self,n):
         a = math.radians(self.angle)
         h = math.sin(a)*n
         w = math.cos(a)*n
         newpos = (self.pos[0]+h,self.pos[1]+w)
-        Drawing.connect_p(self.pos,newpos)
+        Drawing.connect_p(self.pos,newpos,self.ax)
         self.pos = newpos
-        
-    def get_pos(self):
-        return self.pos
-    
-    def get_angle(self):
-        return self.angle
-    
-    def get_draw(self):
-        return self.draw
