9f1a6acf859cf36ae8def8306eae11d1a6bff7f2
SymmetricChaos <ajfraebel@gmail.com>
2020-07-05 09:05:55 -0400

    Update DecisionProblems.py

--- GEB/Chapter8TNT/DecisionProblems.py
+++ GEB/Chapter8TNT/DecisionProblems.py
@@ -4,12 +4,12 @@ from GEB.Chapter8TNT.Properties import is_well_formed
 
 divisibility = "∃c:(a⋅c)=b"
 is_even = "∃a:(a⋅SS0)=b"
-greater_than = "∃c:<(a+c)=b∧~a=b>"
+greater_than = "∃c:(a+Sc)=b"
 prime = "∀a:~∃c:(SSa⋅SSc)=b"
 square = "∃a:(a⋅a)=b"
 square_root = "∃b:(a⋅a)=b"
 power_of_two = "∀a:<∃c:(a⋅c)=b⊃∃d:(d⋅SS0)=a>"
-
+multiple_of_ten = "∃a:∃b:<(SS0⋅a)=c∧(SSSSS0⋅b)=c>"
 
 
 explanations = ["b is divisible by a if",
@@ -18,7 +18,8 @@ explanations = ["b is divisible by a if",
                 "b is prime if",
                 "b is square if",
                 "a is the square root of b if",
-                "b is a power of two if"]
+                "b is a power of two if",
+                "c is a multiple of ten if"]
 
 theorems = [divisibility,
             is_even,
@@ -26,10 +27,11 @@ theorems = [divisibility,
             prime,
             square,
             square_root,
-            power_of_two]
+            power_of_two,
+            multiple_of_ten]
 
 
-
+print("Cast decision problems in terms of open statements of TNT\n\n")
 for ex,th in zip(explanations,theorems):
     if not is_well_formed(th):
         print(f"{th} is not well formed!")
