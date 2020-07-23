94c31a36dd8705b7f8a8396d4db160e1af2dabec
SymmetricChaos <ajfraebel@gmail.com>
2020-07-23 10:23:22 -0400

    Update PrefixTree.py

--- LanguageStuff/PrefixTree.py
+++ LanguageStuff/PrefixTree.py
@@ -1,8 +1,4 @@
-word_list = ["the","thee","there","three",
-             "that","then","than","tower",
-             "tree","trunk","taste","tasteful",
-             "track","trail","train","trip"]
-
+import sys
 
 # A prefix tree is a list of the form
 # [prefix,[prefix trees or "" terminator]]
@@ -22,14 +18,27 @@ def prefix_trees(words):
     return prefix_dict
 
 def words_in_tree(tree,prefix=""):
+    L = []
     for k,v in tree.items():
         new_prefix = prefix+k
         if v == {}:
-            print(new_prefix)
+            L.append(new_prefix)
         else:
-            words_in_tree(tree[k],new_prefix)
+            L += words_in_tree(tree[k],new_prefix)
+    return L
+
+word_list = ["the","thee","there","three",
+             "that","then","than","tower",
+             "tree","trunk","taste","tasteful",
+             "tasty","tastefully","thank","thanks",
+             "thankful","thankfully",
+             "track","trail","train","trip","training"]
 
 
 tree = prefix_trees(word_list)
 print(tree)
-words_in_tree(tree)
+print(words_in_tree(tree))
+
+
+print(sys.getsizeof(word_list))
+print(sys.getsizeof(tree))
