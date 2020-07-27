a124a8c4498d60709124c3596326755b33db4883
SymmetricChaos <ajfraebel@gmail.com>
2020-07-27 17:01:09 -0400

    Update RecognizeString.py

--- LanguageStuff/RecognizeString.py
+++ LanguageStuff/RecognizeString.py
@@ -1,61 +1,8 @@
 import string
 
 
-
-def recognize_letter(letter):
-    if letter in string.ascii_letters:
-        return True
-    return False
-
-
-def recognize_upper_case(letter):
-    if letter in string.ascii_uppercase:
-        return True
-    return False
-
-
-def recognize_lower_case(letter):
-    if letter in string.ascii_lowercase:
-        return True
-    return False
-
-
-def recognize_digit(digit):
-    if digit in string.digits:
-        return True
-    return False
-
-
-def recognize_number(digits):
-    for d in digits:
-        if not recognize_digit(d):
-            return False
-    return True
-
-
-def recognize_simple_name(name):
-    if recognize_upper_case(name[0]):
-        for letter in name[1:]:
-            if not recognize_letter(letter):
-                return False
-        return True
-    return False
-
-
-def recognize_hypen_name(name):
-    parts = name.split("-")
-    for part in parts:
-        if not recognize_simple_name(part):
-            return False
-        return True
-
-# Restricted to less than 3999
-def recognize_roman_numbeal_3999(numeral):
-    
-    # Most basic check
-    for n in numeral:
-        if n not in "IVXLCM":
-            return False
+# Restricted to less than 3999 because standard conventions only go that far
+def recognize_roman_numeral_3999(numeral):
     
     # Atomic roman numerals
     units = ("I","II","III","IV","V","VI","VII","VIII","IX")
@@ -64,37 +11,53 @@ def recognize_roman_numbeal_3999(numeral):
     thous = ("M","MM","MMM")
     all_atoms = units+tens+hunds+thous
     
-    Roman = {"units": units,
-             "tens": tens,
-             "hunds": hunds,
-             "thous": thous}
+    # Roman numeral rank
+    # Returns 4 if invalid to catch order errors
+    def RNR(S):
+        Roman = {"units": units,
+                 "tens": tens,
+                 "hunds": hunds,
+                 "thous": thous}
+        
+        ranks = {"units":0,
+                 "tens":1,
+                 "hunds":2,
+                 "thous":3}
+        
+        for k,v in Roman.items():
+            if S in v:
+                return ranks[k]
+        return 4
     
-    ranks = {"units":0,
-             "tens":1,
-             "hunds":2,
-             "thous":3}
-    
-    left = ""
-    right = numeral
-    ctr = 4
-    while len(right) > 0:
-        # print(left,right)
-        left += right[0]
-        right = right[1:]
-        if left in all_atoms:
+    headptr = 0
+    tailptr = 0
+    cur_rank = 5
+    while tailptr <= len(numeral)+1:
+        tailptr += 1
+        segment = numeral[headptr:tailptr]
+        if segment in all_atoms:
             continue
         else:
-            if left[:-1] in all_atoms:
-                for ranking in ("units","tens","hunds","thous"):
-                    if left[:-1] in Roman[ranking]:
-                        if ranks[ranking] >= ctr:
-                            return False
-                        ctr = ranks[ranking]
-                right = left[-1]+right
-                left = ""
+            prev_num = numeral[headptr:tailptr-1]
+            if prev_num in all_atoms:
+                new_rank = RNR(prev_num)
+                if new_rank >= cur_rank:
+                    # Catch out of order substring
+                    return False
+                else:
+                    cur_rank = new_rank
+                    headptr = tailptr-1
             else:
+                # Catch invalid characters
                 return False
+    
+    # Catch final out of order substring
+    if RNR(segment) >= cur_rank:
+        return False
+        
     return True
 
+
+
 # print(recognize_hypen_name("McDermot-West"))
-print(recognize_roman_numbeal_3999("MXICCCCMIIV"))
+print(recognize_roman_numeral_3999("MIXX"))
